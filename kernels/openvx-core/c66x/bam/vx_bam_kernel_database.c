5cafc4062b4e8300027e17d7141587be48f89339
Jesse Villarreal <jesse.villarreal@ti.com>
2017-03-20 14:10:37 -0500

    [BAM Database] Putting functions in alphabetical order
    Change-Id: I0899fe57cfd3f15ca4224c1d70f23448cc56fb4a
    Signed-off-by: Jesse Villarreal <jesse.villarreal@ti.com>

--- kernels/openvx-core/c66x/bam/vx_bam_kernel_database.c
+++ kernels/openvx-core/c66x/bam/vx_bam_kernel_database.c
@@ -25,14 +25,14 @@ static BAM_KernelHostDBdef bamKernelHostDB[] =
     { &gBAM_TI_dmaAutoIncrementWriteKernel, &gBAM_TI_dmaWriteAutoIncrementKernelHelperFunc, "ti_dma_write_autoincrement", BAM_KERNELID_DMAWRITE_AUTOINCREMENT},
     { &gBAM_TI_dmaOneShotReadKernel, &gBAM_TI_dmaReadOneShotKernelHelperFunc, "ti_dma_read_oneshot", BAM_KERNELID_DMAREAD_ONESHOT},
     { &gBAM_TI_dmaOneShotWriteKernel, &gBAM_TI_dmaWriteOneShotKernelHelperFunc, "ti_dma_write_oneshot", BAM_KERNELID_DMAWRITE_ONESHOT},
+    { &gBAM_VXLIB_absDiff_i8u_i8u_o8u_kernel, &gBAM_VXLIB_absDiff_i8u_i8u_o8u_helperFunc, "vxlib_absDiff_i8u_i8u_o8u", BAM_KERNELID_VXLIB_ABSDIFF_I8U_I8U_O8U },
+    { &gBAM_VXLIB_absDiff_i16s_i16s_o16s_kernel, &gBAM_VXLIB_absDiff_i16s_i16s_o16s_helperFunc, "vxlib_absDiff_i16s_i16s_o16s", BAM_KERNELID_VXLIB_ABSDIFF_I16S_I16S_O16S },
     { &gBAM_VXLIB_histogram_i8u_o32u_kernel, &gBAM_VXLIB_histogram_i8u_o32u_helperFunc, "vxlib_histogram_i8u_o32u", BAM_KERNELID_VXLIB_HISTOGRAM_I8U_O32U },
     { &gBAM_VXLIB_sobel_3x3_i8u_o16s_o16s_kernel, &gBAM_VXLIB_sobel_3x3_i8u_o16s_o16s_helperFunc, "vxlib_sobel_3x3_i8u_o16s_o16s", BAM_KERNELID_VXLIB_SOBEL_3X3_I8U_O16S_O16S },
     { &gBAM_VXLIB_sobelX_3x3_i8u_o16s_kernel, &gBAM_VXLIB_sobelX_3x3_i8u_o16s_helperFunc, "vxlib_sobelX_3x3_i8u_o16s", BAM_KERNELID_VXLIB_SOBELX_3X3_I8U_O16S },
     { &gBAM_VXLIB_sobelY_3x3_i8u_o16s_kernel, &gBAM_VXLIB_sobelY_3x3_i8u_o16s_helperFunc, "vxlib_sobelY_3x3_i8u_o16s", BAM_KERNELID_VXLIB_SOBELY_3X3_I8U_O16S },
     { &gBAM_VXLIB_tableLookup_i16s_o16s_kernel, &gBAM_VXLIB_tableLookup_i16s_o16s_helperFunc, "vxlib_tableLookup_i16s_o16s", BAM_KERNELID_VXLIB_TABLELOOKUP_I16S_O16S },
     { &gBAM_VXLIB_tableLookup_i8u_o8u_kernel, &gBAM_VXLIB_tableLookup_i8u_o8u_helperFunc, "vxlib_tableLookup_i8u_o8u", BAM_KERNELID_VXLIB_TABLELOOKUP_I8U_O8U },
-    { &gBAM_VXLIB_absDiff_i8u_i8u_o8u_kernel, &gBAM_VXLIB_absDiff_i8u_i8u_o8u_helperFunc, "vxlib_absDiff_i8u_i8u_o8u", BAM_KERNELID_VXLIB_ABSDIFF_I8U_I8U_O8U },
-    { &gBAM_VXLIB_absDiff_i16s_i16s_o16s_kernel, &gBAM_VXLIB_absDiff_i16s_i16s_o16s_helperFunc, "vxlib_absDiff_i16s_i16s_o16s", BAM_KERNELID_VXLIB_ABSDIFF_I16S_I16S_O16S },
 };
 
 /**
@@ -47,14 +47,14 @@ static BAM_KernelExecFuncDBdef bamKernelExecFuncDB[] =
     { &gBAM_TI_dmaAutoIncrementWriteKernel, &gBAM_TI_dmaWriteAutoIncrementKernelExecFunc, "ti_dma_write_autoincrement", BAM_KERNELID_DMAWRITE_AUTOINCREMENT},
     { &gBAM_TI_dmaOneShotReadKernel, &gBAM_TI_dmaReadOneShotKernelExecFunc, "ti_dma_read_oneshot", BAM_KERNELID_DMAREAD_ONESHOT},
     { &gBAM_TI_dmaOneShotWriteKernel, &gBAM_TI_dmaWriteOneShotKernelExecFunc, "ti_dma_write_oneshot", BAM_KERNELID_DMAWRITE_ONESHOT},
+    { &gBAM_VXLIB_absDiff_i8u_i8u_o8u_kernel, &gBAM_VXLIB_absDiff_i8u_i8u_o8u_execFunc, "vxlib_absDiff_i8u_i8u_o8u", BAM_KERNELID_VXLIB_ABSDIFF_I8U_I8U_O8U },
+    { &gBAM_VXLIB_absDiff_i16s_i16s_o16s_kernel, &gBAM_VXLIB_absDiff_i16s_i16s_o16s_execFunc, "vxlib_absDiff_i16s_i16s_o16s", BAM_KERNELID_VXLIB_ABSDIFF_I16S_I16S_O16S },
     { &gBAM_VXLIB_histogram_i8u_o32u_kernel, &gBAM_VXLIB_histogram_i8u_o32u_execFunc, "vxlib_histogram_i8u_o32u", BAM_KERNELID_VXLIB_HISTOGRAM_I8U_O32U },
     { &gBAM_VXLIB_sobel_3x3_i8u_o16s_o16s_kernel, &gBAM_VXLIB_sobel_3x3_i8u_o16s_o16s_execFunc, "vxlib_sobel_3x3_i8u_o16s_o16s", BAM_KERNELID_VXLIB_SOBEL_3X3_I8U_O16S_O16S },
     { &gBAM_VXLIB_sobelX_3x3_i8u_o16s_kernel, &gBAM_VXLIB_sobelX_3x3_i8u_o16s_execFunc, "vxlib_sobelX_3x3_i8u_o16s", BAM_KERNELID_VXLIB_SOBELX_3X3_I8U_O16S },
     { &gBAM_VXLIB_sobelY_3x3_i8u_o16s_kernel, &gBAM_VXLIB_sobelY_3x3_i8u_o16s_execFunc, "vxlib_sobelY_3x3_i8u_o16s", BAM_KERNELID_VXLIB_SOBELY_3X3_I8U_O16S },
     { &gBAM_VXLIB_tableLookup_i16s_o16s_kernel, &gBAM_VXLIB_tableLookup_i16s_o16s_execFunc, "vxlib_tableLookup_i16s_o16s", BAM_KERNELID_VXLIB_TABLELOOKUP_I16S_O16S },
     { &gBAM_VXLIB_tableLookup_i8u_o8u_kernel, &gBAM_VXLIB_tableLookup_i8u_o8u_execFunc, "vxlib_tableLookup_i8u_o8u", BAM_KERNELID_VXLIB_TABLELOOKUP_I8U_O8U },
-    { &gBAM_VXLIB_absDiff_i8u_i8u_o8u_kernel, &gBAM_VXLIB_absDiff_i8u_i8u_o8u_execFunc, "vxlib_absDiff_i8u_i8u_o8u", BAM_KERNELID_VXLIB_ABSDIFF_I8U_I8U_O8U },
-    { &gBAM_VXLIB_absDiff_i16s_i16s_o16s_kernel, &gBAM_VXLIB_absDiff_i16s_i16s_o16s_execFunc, "vxlib_absDiff_i16s_i16s_o16s", BAM_KERNELID_VXLIB_ABSDIFF_I16S_I16S_O16S },
 };
 
 BAM_KernelDBdef gBAM_TI_kernelDBdef =
