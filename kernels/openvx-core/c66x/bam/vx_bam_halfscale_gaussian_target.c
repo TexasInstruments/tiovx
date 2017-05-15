02f6a7109b846765524b9292db30c01401a5f2ff
Jesse Villarreal <jesse.villarreal@ti.com>
2017-05-15 09:41:27 -0500

    [BAM] Fix bam support for half scale gaussian
    Change-Id: Ib3bb7eaaa60b96798f5a1d5947664416aaffd628
    Signed-off-by: Jesse Villarreal <jesse.villarreal@ti.com>

--- kernels/openvx-core/c66x/bam/vx_bam_halfscale_gaussian_target.c
+++ kernels/openvx-core/c66x/bam/vx_bam_halfscale_gaussian_target.c
@@ -16,9 +16,24 @@
 #include <tivx_kernel_utils.h>
 #include <vx_bam_kernel_wrapper.h>
 
+/* In the case of gaussian size == 3, use 2 separate graphs, one for
+ * gaussian 3x3, and another for half scale.  A single graph for these
+ * two is not yet supported. */
+#define TWO_BAM_GRAPHS   1
+
+#define SOURCE_NODE      0
+#define GAUSSIAN_NODE    1
+#define SCALE_NODE       2
+#define SINK_NODE        3
+
 typedef struct
 {
     tivx_bam_graph_handle graph_handle;
+#if TWO_BAM_GRAPHS
+    tivx_bam_graph_handle graph_handleScale;
+    uint8_t *scratch;
+    uint32_t scratchSize;
+#endif
 } tivxHalfScaleGaussianParams;
 
 static tivx_target_kernel vx_halfscale_gaussian_target_kernel = NULL;
@@ -103,23 +118,41 @@ static vx_status VX_CALLBACK tivxBamKernelHalfScaleGaussianProcess(
 
         if (gsize_value == 1)
         {
+            img_ptrs[1] = dst_addr;
         }
         else if (gsize_value == 3)
         {
+#if TWO_BAM_GRAPHS
+            img_ptrs[1] = (vx_uint8*)(prms->scratch + src->imagepatch_addr[0].stride_y + 1);
+#else
+            img_ptrs[1] = dst_addr;
+#endif
+
         }
         else if (gsize_value == 5)
         {
             dst_addr = (vx_uint8*)(dst_addr + dst->imagepatch_addr[0].stride_y + 1);
+            img_ptrs[1] = dst_addr;
         }
         else
         {
         }
 
         img_ptrs[0] = src_addr;
-        img_ptrs[1] = dst_addr;
         tivxBamUpdatePointers(prms->graph_handle, 1U, 1U, img_ptrs);
 
         status  = tivxBamProcessGraph(prms->graph_handle);
+
+#if TWO_BAM_GRAPHS
+        if(gsize_value == 3)
+        {
+            img_ptrs[0] = prms->scratch;
+            img_ptrs[1] = dst_addr;
+            tivxBamUpdatePointers(prms->graph_handleScale, 1U, 1U, img_ptrs);
+
+            status  = tivxBamProcessGraph(prms->graph_handleScale);
+        }
+#endif
 
         tivxMemBufferUnmap(src->mem_ptr[0U].target_ptr, src->mem_size[0],
             src->mem_ptr[0U].mem_type, VX_READ_ONLY);
@@ -203,23 +236,119 @@ static vx_status VX_CALLBACK tivxBamKernelHalfScaleGaussianCreate(
                     buf_params, &kernel_details,
                     &prms->graph_handle);
             }
-            else if ((gsize_value == 3) || (gsize_value == 5))
+            else if (3 == gsize_value)
             {
-                if (gsize_value == 5)
-                {
-                    vxlib_dst.dim_x -= 2;
-                    vxlib_dst.dim_y -= 2;
+#if TWO_BAM_GRAPHS
+                VXLIB_bufParams2D_t vxlib_int;
+                BAM_VXLIB_scaleImageNearest_i8u_o8u_params kernel_params;
 
-                    kernel_details.compute_kernel_params = NULL;
+                prms->scratchSize = vxlib_src.dim_y*vxlib_src.stride_y;
+                prms->scratch = tivxMemAlloc(prms->scratchSize, TIVX_MEM_EXTERNAL);
 
-                    BAM_VXLIB_halfScaleGaussian_5x5_i8u_o8u_getKernelInfo(
-                        NULL, &kernel_details.kernel_info);
+                kernel_params.xScale = 2;
+                kernel_params.yScale = 2;
+                kernel_params.srcOffsetX = 0;
+                kernel_params.srcOffsetY = 0;
+                kernel_params.dstOffsetX = 0;
+                kernel_params.dstOffsetY = 0;
 
-                    status = tivxBamCreateHandleSingleNode(
-                        BAM_KERNELID_VXLIB_HALFSCALEGAUSSIAN_5x5_I8U_O8U,
-                        buf_params, &kernel_details,
-                        &prms->graph_handle);
-                }
+                kernel_details.compute_kernel_params = (void*)&kernel_params;
+
+                vxlib_int.dim_x = src->imagepatch_addr[0].dim_x-2;
+                vxlib_int.dim_y = src->imagepatch_addr[0].dim_y-2;
+                vxlib_int.stride_y = src->imagepatch_addr[0].stride_y;
+                vxlib_int.data_type = VXLIB_UINT8;
+
+                buf_params[1] = &vxlib_int;
+
+                BAM_VXLIB_gaussian_3x3_i8u_o8u_getKernelInfo(
+                    NULL, &kernel_details.kernel_info);
+
+                status = tivxBamCreateHandleSingleNode(
+                    BAM_KERNELID_VXLIB_GAUSSIAN_3X3_I8U_O8U,
+                    buf_params, &kernel_details,
+                    &prms->graph_handle);
+
+                vxlib_int.dim_x = src->imagepatch_addr[0].dim_x;
+                vxlib_int.dim_y = src->imagepatch_addr[0].dim_y;
+
+                buf_params[0] = &vxlib_int;
+                buf_params[1] = &vxlib_dst;
+
+                BAM_VXLIB_scaleImageNearest_i8u_o8u_getKernelInfo(
+                    &kernel_params, &kernel_details.kernel_info);
+
+                status = tivxBamCreateHandleSingleNode(
+                    BAM_KERNELID_VXLIB_SCALEIMAGENEARES_I8U_O8U,
+                    buf_params, &kernel_details,
+                    &prms->graph_handleScale);
+
+#else
+                tivx_bam_kernel_details_t kernel_details_g3[4];
+                BAM_VXLIB_scaleImageNearest_i8u_o8u_params kernel_params;
+
+                BAM_NodeParams node_list[] = { \
+                    {SOURCE_NODE, BAM_KERNELID_DMAREAD_AUTOINCREMENT, NULL}, \
+                    {GAUSSIAN_NODE, BAM_KERNELID_VXLIB_GAUSSIAN_3X3_I8U_O8U, NULL}, \
+                    {SCALE_NODE, BAM_KERNELID_VXLIB_SCALEIMAGENEARES_I8U_O8U, NULL}, \
+                    {SINK_NODE, BAM_KERNELID_DMAWRITE_AUTOINCREMENT, NULL}, \
+                    {BAM_END_NODE_MARKER,   0,                          NULL},\
+                };
+
+                BAM_EdgeParams edge_list[]= {\
+                    {{SOURCE_NODE, 0},
+                        {GAUSSIAN_NODE, BAM_VXLIB_GAUSSIAN_3X3_I8U_O8U_INPUT_IMAGE_PORT}},\
+
+                    {{GAUSSIAN_NODE, BAM_VXLIB_GAUSSIAN_3X3_I8U_O8U_OUTPUT_IMAGE_PORT},
+                        {SCALE_NODE, BAM_VXLIB_SCALEIMAGENEAREST_I8U_O8U_INPUT_IMAGE_PORT}},\
+
+                    {{SCALE_NODE, BAM_VXLIB_SCALEIMAGENEAREST_I8U_O8U_OUTPUT_IMAGE_PORT},
+                        {SINK_NODE, 0}},\
+
+                    {{BAM_END_NODE_MARKER, 0},
+                        {BAM_END_NODE_MARKER, 0}},\
+                };
+
+                vxlib_dst.dim_x -= 2;
+                vxlib_dst.dim_y -= 2;
+
+                kernel_params.xScale = 2;
+                kernel_params.yScale = 2;
+                kernel_params.srcOffsetX = 1;
+                kernel_params.srcOffsetY = 1;
+                kernel_params.dstOffsetX = 1;
+                kernel_params.dstOffsetY = 1;
+
+                BAM_VXLIB_gaussian_3x3_i8u_o8u_getKernelInfo(
+                    NULL, &kernel_details_g3[GAUSSIAN_NODE].kernel_info);
+
+                BAM_VXLIB_scaleImageNearest_i8u_o8u_getKernelInfo(
+                    &kernel_params, &kernel_details_g3[SCALE_NODE].kernel_info);
+
+                kernel_details_g3[SOURCE_NODE].compute_kernel_params = NULL;
+                kernel_details_g3[GAUSSIAN_NODE].compute_kernel_params = NULL;
+                kernel_details_g3[SCALE_NODE].compute_kernel_params = (void*)&kernel_params;
+                kernel_details_g3[SINK_NODE].compute_kernel_params = NULL;
+
+                status = tivxBamCreateHandleMultiNode(node_list, edge_list,
+                                                      buf_params, kernel_details_g3,
+                                                      &prms->graph_handle);
+#endif
+            }
+            else if (5 == gsize_value)
+            {
+                vxlib_dst.dim_x -= 2;
+                vxlib_dst.dim_y -= 2;
+
+                kernel_details.compute_kernel_params = NULL;
+
+                BAM_VXLIB_halfScaleGaussian_5x5_i8u_o8u_getKernelInfo(
+                    NULL, &kernel_details.kernel_info);
+
+                status = tivxBamCreateHandleSingleNode(
+                    BAM_KERNELID_VXLIB_HALFSCALEGAUSSIAN_5x5_I8U_O8U,
+                    buf_params, &kernel_details,
+                    &prms->graph_handle);
             }
             else
             {
@@ -255,6 +384,7 @@ static vx_status VX_CALLBACK tivxBamKernelHalfScaleGaussianDelete(
     vx_status status = VX_SUCCESS;
     uint32_t size;
     tivxHalfScaleGaussianParams *prms = NULL;
+    tivx_obj_desc_scalar_t *gsize;
 
     status = ownCheckNullParams(obj_desc, num_params,
                 TIVX_KERNEL_HALFSCALE_GAUSSIAN_MAX_PARAMS);
@@ -268,6 +398,20 @@ static vx_status VX_CALLBACK tivxBamKernelHalfScaleGaussianDelete(
             (sizeof(tivxHalfScaleGaussianParams) == size))
         {
             tivxBamDestroyHandle(prms->graph_handle);
+
+#if TWO_BAM_GRAPHS
+            gsize = (tivx_obj_desc_scalar_t *)obj_desc[
+                TIVX_KERNEL_HALFSCALE_GAUSSIAN_GSIZE_IDX];
+
+            if( gsize != NULL)
+            {
+                if(gsize->data.s32 == 3)
+                {
+                    tivxBamDestroyHandle(prms->graph_handleScale);
+                    tivxMemFree(prms->scratch, prms->scratchSize, TIVX_MEM_EXTERNAL);
+                }
+            }
+#endif
             tivxMemFree(prms, sizeof(tivxHalfScaleGaussianParams), TIVX_MEM_EXTERNAL);
         }
     }
