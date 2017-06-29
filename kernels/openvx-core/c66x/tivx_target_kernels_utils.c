dcbe4e2ab965b730978bd735ef97b4040024d6e4
Lucas Weaver <l-weaver@ti.com>
2017-06-29 17:16:35 -0500

    [COMMON FUNCTION] Change to ownInitBufParams
    -Modifying to remove any user-set padding values
    -Approved by Brijesh
    -Will modify calls to this function in next patch
    
    Change-Id: I8f89aa50d5f41613787edf520b23990c8ec07551

--- kernels/openvx-core/c66x/tivx_target_kernels_utils.c
+++ kernels/openvx-core/c66x/tivx_target_kernels_utils.c
@@ -112,52 +112,30 @@ void ownReserveC66xL2MEM(void)
 
 void ownInitBufParams(
     tivx_obj_desc_image_t *obj_desc,
-    vx_rectangle_t *rect,
-    VXLIB_bufParams2D_t buf_params[],
-    uint32_t lpad, uint32_t tpad, uint32_t rpad, uint32_t bpad)
+    VXLIB_bufParams2D_t buf_params[])
 {
     uint32_t i;
 
-    if (NULL == rect)
-    {
-        rect = &obj_desc->valid_roi;
-    }
-
-    if ((rect->start_x == 0u) && (lpad != 0u))
-    {
-        lpad = 0;
-    }
-    if ((rect->start_y == 0u) && (tpad != 0u))
-    {
-        tpad = 0;
-    }
-    if ((rect->end_x == obj_desc->imagepatch_addr[0].dim_x) && (rpad != 0u))
-    {
-        rpad = 0;
-    }
-    if ((rect->end_y == obj_desc->imagepatch_addr[0].dim_y) && (bpad != 0u))
-    {
-        bpad = 0;
-    }
-
     for (i = 0; i < obj_desc->planes; i ++)
     {
-        buf_params[i].dim_x = (rect->end_x + rpad) - (rect->start_x - lpad);
-        buf_params[i].dim_y = (rect->end_y + bpad) - (rect->start_y - tpad);
+        buf_params[i].dim_x = obj_desc->valid_roi.end_x - obj_desc->valid_roi.start_x;
+        buf_params[i].dim_y = obj_desc->valid_roi.end_y - obj_desc->valid_roi.start_y;
         buf_params[i].stride_y = obj_desc->imagepatch_addr[i].stride_y;
 
-        if (512 == obj_desc->imagepatch_addr[1].scale_x)
+        if (512 == obj_desc->imagepatch_addr[i].scale_x)
         {
-            buf_params[i].dim_x = buf_params[i].dim_x / 2;
             buf_params[i].dim_y = buf_params[i].dim_y / 2;
+
+            if (VX_DF_IMAGE_IYUV == obj_desc->format)
+            {
+                buf_params[i].dim_x = buf_params[i].dim_x / 2;
+            }
         }
 
         switch(obj_desc->format)
         {
             case VX_DF_IMAGE_NV12:
             case VX_DF_IMAGE_NV21:
-            case VX_DF_IMAGE_UYVY:
-            case VX_DF_IMAGE_YUYV:
             case VX_DF_IMAGE_IYUV:
             case VX_DF_IMAGE_YUV4:
             case VX_DF_IMAGE_U8:
@@ -178,6 +156,10 @@ void ownInitBufParams(
                 break;
             case VX_DF_IMAGE_RGB:
                 buf_params[i].data_type = VXLIB_UINT24;
+                break;
+            case VX_DF_IMAGE_YUYV:
+            case VX_DF_IMAGE_UYVY:
+                buf_params[i].data_type = VXLIB_UINT16;
                 break;
         }
     }
