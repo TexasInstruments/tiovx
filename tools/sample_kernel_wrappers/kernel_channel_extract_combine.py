da520d8e1c72db10c304c8f5f5050e25c34346d4
Lucas Weaver <l-weaver@ti.com>
2018-03-23 16:00:36 -0500

    [TIOVX-218] Adding sample kernel wrappers including a superset wrapper

---
+++ tools/sample_kernel_wrappers/kernel_channel_extract_combine.py
@@ -0,0 +1,35 @@
+'''
+* Copyright (C) 2017 Texas Instruments Incorporated - http://www.ti.com/
+* ALL RIGHTS RESERVED
+'''
+
+from tiovx import *
+
+code = KernelExportCode("TI", "extvx", "ext1", "c66", "CUSTOM_APPLICATION_PATH")
+
+kernel = Kernel("channel_extract")
+
+kernel.setParameter(Type.IMAGE, Direction.INPUT, ParamState.REQUIRED, "IN", do_map=False, do_unmap=False)
+kernel.setParameter(Type.ENUM, Direction.INPUT, ParamState.REQUIRED, "CHANNEL")
+kernel.setParameter(Type.IMAGE, Direction.OUTPUT, ParamState.REQUIRED, "OUT")
+
+kernel.setTarget(Target.DSP1)
+kernel.setTarget(Target.DSP2)
+
+code.export(kernel)
+
+
+
+
+kernel = Kernel("channel_combine")
+
+kernel.setParameter(Type.IMAGE, Direction.INPUT, ParamState.REQUIRED, "SRC0")
+kernel.setParameter(Type.IMAGE, Direction.INPUT, ParamState.REQUIRED, "SRC1")
+kernel.setParameter(Type.IMAGE, Direction.INPUT, ParamState.OPTIONAL, "SRC2")
+kernel.setParameter(Type.IMAGE, Direction.INPUT, ParamState.OPTIONAL, "SRC3")
+kernel.setParameter(Type.IMAGE, Direction.OUTPUT, ParamState.REQUIRED, "DST", do_map_unmap_all_planes=True)
+
+kernel.setTarget(Target.DSP1)
+kernel.setTarget(Target.DSP2)
+
+code.export(kernel)
