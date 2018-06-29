e9246b041897ebe62a817eec8830b3f106fc7017
Jesse Villarreal <jesse.villarreal@ti.com>
2018-06-29 00:03:57 -0500

    [Documentation] Removing timestamps, and other cleanup activities
    - Removing timestamps so that every file doesn't change when we generate
      documentation.
    - Fixing the TI logo missing issue
    - Fixing the doxygen warnings
    - Moving module definitions to its own modules.h file

--- kernels_tda4x/include/TI/tda4x_kernels.h
+++ kernels_tda4x/include/TI/tda4x_kernels.h
@@ -85,20 +85,6 @@ extern "C" {
  */
 #define TIVX_MODULE_NAME_TIDL    "tidl"
 
-/*! \brief The list of kernels supported in hwa module
- *
- * Each kernel listed here can be used with the <tt>\ref vxGetKernelByName</tt> call.
- * When programming the parameters, use
- * \arg <tt>\ref VX_INPUT</tt> for [in]
- * \arg <tt>\ref VX_OUTPUT</tt> for [out]
- * \arg <tt>\ref VX_BIDIRECTIONAL</tt> for [in,out]
- *
- * When programming the parameters, use
- * \arg <tt>\ref VX_TYPE_IMAGE</tt> for a <tt>\ref vx_image</tt> in the size field of <tt>\ref vxGetParameterByIndex</tt> or <tt>\ref vxSetParameterByIndex</tt>
- * \arg <tt>\ref VX_TYPE_ARRAY</tt> for a <tt>\ref vx_array</tt> in the size field of <tt>\ref vxGetParameterByIndex</tt> or <tt>\ref vxSetParameterByIndex</tt>
- * \arg or other appropriate types in \ref vx_type_e.
- * \ingroup group_vision_function_hwa
- */
 
 /*! \brief vpac_nf_generic kernel name
  *  \ingroup group_vision_function_vpac_nf
