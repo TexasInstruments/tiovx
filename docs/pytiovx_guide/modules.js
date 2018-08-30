4473d29b13ea1b445769cfdded223eec159e4ddd
Lucas Weaver <l-weaver@ti.com>
2018-08-30 13:03:23 -0500

    [PYTIOVX DOC] Adding table of contents for PYTIOVX documentation

--- docs/pytiovx_guide/modules.js
+++ docs/pytiovx_guide/modules.js
@@ -1,4 +1,8 @@
 var modules =
 [
+    [ "Installation", "index.html#INSTALLATION", null ],
+    [ "Usage", "index.html#USAGE", null ],
+    [ "Kernel Generation", "index.html#KERNEL_SCRIPT", null ],
+    [ "Use Case Generation", "index.html#USE_CASE_SCRIPT", null ],
     [ "PyTIOVX APIs", "group__PYTIOVX__API.html", "group__PYTIOVX__API" ]
 ];
