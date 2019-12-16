TexasInstruments/tiovx

commit af0dbb22d90a7895167d27d2aa75b3e7ceef3500

[TIOVX-56] MISRAC: R10.3 ETYPE (casting VXLIB data types to uint32_t)
- Casting of uint32_t for VXLIB data types
  - Covers *some* of KW issues for
    - R10.4 (MISRA.ETYPE.CATEGORY.DIFFERENT.2012

kernels/openvx-core/c66x/bam/vx_bam_harris_corners_target.c
@@ -413,8 +413,8 @@ static vx_status VX_CALLBACK tivxKernelHarrisCornersCreate(
             prms->vxlib_score.dim_y = img->imagepatch_addr[0].dim_y -
                 (prms->gs - 1) - (prms->bs - 1);
             prms->vxlib_score.stride_y =
-                img->imagepatch_addr[0].dim_x * sizeof(VXLIB_FLOAT32);
-            prms->vxlib_score.data_type = VXLIB_FLOAT32;
+                img->imagepatch_addr[0].dim_x * sizeof((uint32_t)VXLIB_FLOAT32);
+            prms->vxlib_score.data_type = (uint32_t)VXLIB_FLOAT32;
 
             prms->hcs_score_size = prms->vxlib_score.stride_y *
                 prms->vxlib_score.dim_y;

kernels/openvx-core/c66x/bam/vx_bam_intgimg_target.c
@@ -205,7 +205,7 @@ static vx_status VX_CALLBACK tivxKernelIntgImgCreate(
             BAM_VXLIB_integralImage_i8u_o32u_getKernelInfo( &kernel_params,
                                                         &kernel_details.kernel_info);
 
-            status = tivxBamCreateHandleSingleNode(BAM_KERNELID_VXLIB_INTEGRALIMAGE_I8U_O32U,
+            status = tivxBamCreateHandleSingleNode(BAM_KERNELID_(uint32_t)VXLIB_INTEGRALIMAGE_I8U_O32U,
                                                    buf_params, &kernel_details,
                                                    &prms->graph_handle);
         }
@@ -342,7 +342,7 @@ static vx_status VX_CALLBACK tivxKernelIntgImgCreateInBamGraph(
             memset(prms, 0, sizeof(tivxIntgImgParams));
 
             node_list[*bam_node_cnt].nodeIndex = *bam_node_cnt;
-            node_list[*bam_node_cnt].kernelId = BAM_KERNELID_VXLIB_INTEGRALIMAGE_I8U_O32U;
+            node_list[*bam_node_cnt].kernelId = BAM_KERNELID_(uint32_t)VXLIB_INTEGRALIMAGE_I8U_O32U;
             node_list[*bam_node_cnt].kernelArgs = NULL;
 
             kernel_params->frameWidth   = dst->valid_roi.end_x - dst->valid_roi.start_x;
@@ -394,11 +394,11 @@ static vx_status VX_CALLBACK tivxKernelIntgImgGetNodePort(
         {
             case TIVX_KERNEL_INTG_IMG_INPUT_IDX:
                 *bam_node = prms->bam_node_num;
-                *bam_port = BAM_VXLIB_INTEGRALIMAGE_I8U_O32U_INPUT_IMAGE_PORT;
+                *bam_port = BAM_(uint32_t)VXLIB_INTEGRALIMAGE_I8U_O32U_INPUT_IMAGE_PORT;
                 break;
             case TIVX_KERNEL_INTG_IMG_OUTPUT_IDX:
                 *bam_node = prms->bam_node_num;
-                *bam_port = BAM_VXLIB_INTEGRALIMAGE_I8U_O32U_OUTPUT_IMAGE_PORT;
+                *bam_port = BAM_(uint32_t)VXLIB_INTEGRALIMAGE_I8U_O32U_OUTPUT_IMAGE_PORT;
                 break;
             default:
                 VX_PRINT(VX_ZONE_ERROR,"tivxKernelIntgImgGetNodePort: non existing index queried by tivxKernelSupernodeCreate.tivxGetNodePort()\n");

kernels/common/target/tivx_kernels_target_utils.c
@@ -124,27 +124,27 @@ void tivxInitBufParams(
             case (vx_df_image)VX_DF_IMAGE_IYUV:
             case (vx_df_image)VX_DF_IMAGE_YUV4:
             case (vx_df_image)VX_DF_IMAGE_U8:
-                buf_params[i].data_type = VXLIB_UINT8;
+                buf_params[i].data_type = (uint32_t)VXLIB_UINT8;
                 break;
             case (vx_df_image)VX_DF_IMAGE_U16:
-                buf_params[i].data_type = VXLIB_UINT16;
+                buf_params[i].data_type = (uint32_t)VXLIB_UINT16;
                 break;
             case (vx_df_image)VX_DF_IMAGE_S16:
-                buf_params[i].data_type = VXLIB_INT16;
+                buf_params[i].data_type = (uint32_t)VXLIB_INT16;
                 break;
             case (vx_df_image)VX_DF_IMAGE_RGBX:
             case (vx_df_image)VX_DF_IMAGE_U32:
-                buf_params[i].data_type = VXLIB_UINT32;
+                buf_params[i].data_type = (uint32_t)VXLIB_UINT32;
                 break;
             case (vx_df_image)VX_DF_IMAGE_S32:
-                buf_params[i].data_type = VXLIB_INT32;
+                buf_params[i].data_type = (uint32_t)VXLIB_INT32;
                 break;
             case (vx_df_image)VX_DF_IMAGE_RGB:
-                buf_params[i].data_type = VXLIB_UINT24;
+                buf_params[i].data_type = (uint32_t)VXLIB_UINT24;
                 break;
             case (vx_df_image)VX_DF_IMAGE_YUYV:
             case (vx_df_image)VX_DF_IMAGE_UYVY:
-                buf_params[i].data_type = VXLIB_UINT16;
+                buf_params[i].data_type = (uint32_t)VXLIB_UINT16;
                 break;
             default:
                 /* do nothing */
@@ -219,27 +219,27 @@ void tivxInitTwoBufParams(
             case (vx_df_image)VX_DF_IMAGE_IYUV:
             case (vx_df_image)VX_DF_IMAGE_YUV4:
             case (vx_df_image)VX_DF_IMAGE_U8:
-                buf_params0[i].data_type = VXLIB_UINT8;
+                buf_params0[i].data_type = (uint32_t)VXLIB_UINT8;
                 break;
             case (vx_df_image)VX_DF_IMAGE_U16:
-                buf_params0[i].data_type = VXLIB_UINT16;
+                buf_params0[i].data_type = (uint32_t)VXLIB_UINT16;
                 break;
             case (vx_df_image)VX_DF_IMAGE_S16:
-                buf_params0[i].data_type = VXLIB_INT16;
+                buf_params0[i].data_type = (uint32_t)VXLIB_INT16;
                 break;
             case (vx_df_image)VX_DF_IMAGE_RGBX:
             case (vx_df_image)VX_DF_IMAGE_U32:
-                buf_params0[i].data_type = VXLIB_UINT32;
+                buf_params0[i].data_type = (uint32_t)VXLIB_UINT32;
                 break;
             case (vx_df_image)VX_DF_IMAGE_S32:
-                buf_params0[i].data_type = VXLIB_INT32;
+                buf_params0[i].data_type = (uint32_t)VXLIB_INT32;
                 break;
             case (vx_df_image)VX_DF_IMAGE_RGB:
-                buf_params0[i].data_type = VXLIB_UINT24;
+                buf_params0[i].data_type = (uint32_t)VXLIB_UINT24;
                 break;
             case (vx_df_image)VX_DF_IMAGE_YUYV:
             case (vx_df_image)VX_DF_IMAGE_UYVY:
-                buf_params0[i].data_type = VXLIB_UINT16;
+                buf_params0[i].data_type = (uint32_t)VXLIB_UINT16;
                 break;
             default:
                 /* do nothing */
@@ -253,27 +253,27 @@ void tivxInitTwoBufParams(
             case (vx_df_image)VX_DF_IMAGE_IYUV:
             case (vx_df_image)VX_DF_IMAGE_YUV4:
             case (vx_df_image)VX_DF_IMAGE_U8:
-                buf_params1[i].data_type = VXLIB_UINT8;
+                buf_params1[i].data_type = (uint32_t)VXLIB_UINT8;
                 break;
             case (vx_df_image)VX_DF_IMAGE_U16:
-                buf_params1[i].data_type = VXLIB_UINT16;
+                buf_params1[i].data_type = (uint32_t)VXLIB_UINT16;
                 break;
             case (vx_df_image)VX_DF_IMAGE_S16:
-                buf_params1[i].data_type = VXLIB_INT16;
+                buf_params1[i].data_type = (uint32_t)VXLIB_INT16;
                 break;
             case (vx_df_image)VX_DF_IMAGE_RGBX:
             case (vx_df_image)VX_DF_IMAGE_U32:
-                buf_params1[i].data_type = VXLIB_UINT32;
+                buf_params1[i].data_type = (uint32_t)VXLIB_UINT32;
                 break;
             case (vx_df_image)VX_DF_IMAGE_S32:
-                buf_params1[i].data_type = VXLIB_INT32;
+                buf_params1[i].data_type = (uint32_t)VXLIB_INT32;
                 break;
             case (vx_df_image)VX_DF_IMAGE_RGB:
-                buf_params1[i].data_type = VXLIB_UINT24;
+                buf_params1[i].data_type = (uint32_t)VXLIB_UINT24;
                 break;
             case (vx_df_image)VX_DF_IMAGE_YUYV:
             case (vx_df_image)VX_DF_IMAGE_UYVY:
-                buf_params1[i].data_type = VXLIB_UINT16;
+                buf_params1[i].data_type = (uint32_t)VXLIB_UINT16;
                 break;
             default:
                 /* do nothing */

kernels/openvx-core/c66x/bam/vx_bam_absdiff_target.c
@@ -232,7 +232,7 @@ static vx_status VX_CALLBACK tivxKernelAbsDiffCreate(
             buf_params[1] = &vxlib_src1;
             buf_params[2] = &vxlib_dst;
 
-            if (VXLIB_UINT8 == vxlib_dst.data_type)
+            if ((uint32_t)VXLIB_UINT8 == vxlib_dst.data_type)
             {
                 BAM_VXLIB_absDiff_i8u_i8u_o8u_getKernelInfo( NULL,
                                                              &kernel_details.kernel_info);

kernels/openvx-core/c66x/bam/vx_bam_add_target.c
@@ -225,7 +225,7 @@ static vx_status VX_CALLBACK tivxKernelAddCreate(
 
             /* If output is in U8 format, both the input must be in
                U8 format */
-            if (VXLIB_UINT8 == vxlib_dst.data_type)
+            if ((uint32_t)VXLIB_UINT8 == vxlib_dst.data_type)
             {
                 BAM_VXLIB_add_i8u_i8u_o8u_params kernel_params;
 
@@ -248,8 +248,8 @@ static vx_status VX_CALLBACK tivxKernelAddCreate(
                     &kernel_details, &prms->graph_handle);
             }
             /* Now if the both inputs are U8, output will be in S16 format */
-            else if ((VXLIB_UINT8 == vxlib_src1.data_type) &&
-                     (VXLIB_UINT8 == vxlib_src0.data_type))
+            else if (((uint32_t)VXLIB_UINT8 == vxlib_src1.data_type) &&
+                     ((uint32_t)VXLIB_UINT8 == vxlib_src0.data_type))
             {
 
                 BAM_VXLIB_add_i8u_i8u_o16s_getKernelInfo(NULL,
@@ -261,8 +261,8 @@ static vx_status VX_CALLBACK tivxKernelAddCreate(
             }
             /* If both the input are in S16 format, output will be in
                S16 format */
-            else if ((VXLIB_INT16 == vxlib_src1.data_type) &&
-                     (VXLIB_INT16 == vxlib_src0.data_type))
+            else if (((uint32_t)VXLIB_INT16 == vxlib_src1.data_type) &&
+                     ((uint32_t)VXLIB_INT16 == vxlib_src0.data_type))
             {
                 BAM_VXLIB_add_i16s_i16s_o16s_params kernel_params;
 
@@ -297,7 +297,7 @@ static vx_status VX_CALLBACK tivxKernelAddCreate(
                     kernel_params.overflow_policy = VXLIB_CONVERT_POLICY_WRAP;
                 }
 
-                if (VXLIB_UINT8 == vxlib_src0.data_type)
+                if ((uint32_t)VXLIB_UINT8 == vxlib_src0.data_type)
                 {
                     /* Fill in the frame level sizes of buffers here. If the port
                      * is optionally disabled, put NULL */

kernels/openvx-core/c66x/bam/vx_bam_channel_combine_target.c
@@ -342,7 +342,7 @@ static vx_status VX_CALLBACK tivxKernelChannelCombineCreate(
 
                 if( dst->format == (vx_df_image)VX_DF_IMAGE_RGB)
                 {
-                    vxlib_dst[0].data_type = VXLIB_UINT24;
+                    vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT24;
                     buf_params[3] = &vxlib_dst[0];
 
                     BAM_VXLIB_channelCombine_3to1_i8u_o8u_getKernelInfo(NULL,
@@ -355,7 +355,7 @@ static vx_status VX_CALLBACK tivxKernelChannelCombineCreate(
                 else
                 if( dst->format == (vx_df_image)VX_DF_IMAGE_RGBX)
                 {
-                    vxlib_dst[0].data_type = VXLIB_UINT32;
+                    vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT32;
                     buf_params[4] = &vxlib_dst[0];
 
                     BAM_VXLIB_channelCombine_4to1_i8u_o8u_getKernelInfo(NULL,
@@ -368,7 +368,7 @@ static vx_status VX_CALLBACK tivxKernelChannelCombineCreate(
                 else
                 if( dst->format == (vx_df_image)VX_DF_IMAGE_YUYV)
                 {
-                    vxlib_dst[0].data_type = VXLIB_UINT16;
+                    vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT16;
 
                     BAM_VXLIB_channelCombine_yuyv_i8u_o8u_params kernel_params;
 
@@ -387,7 +387,7 @@ static vx_status VX_CALLBACK tivxKernelChannelCombineCreate(
                 }
                 else /* format is (vx_df_image)VX_DF_IMAGE_UYVY */
                 {
-                    vxlib_dst[0].data_type = VXLIB_UINT16;
+                    vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT16;
 
                     BAM_VXLIB_channelCombine_yuyv_i8u_o8u_params kernel_params;
 
@@ -431,7 +431,7 @@ static vx_status VX_CALLBACK tivxKernelChannelCombineCreate(
                         /dst->imagepatch_addr[plane_idx].step_y;
                     vxlib_dst[0].stride_y =
                         dst->imagepatch_addr[plane_idx].stride_y;
-                    vxlib_dst[0].data_type = VXLIB_UINT8;
+                    vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT8;
 
                     buf_params[1] = &vxlib_dst[0];
 
@@ -548,7 +548,7 @@ static vx_status VX_CALLBACK tivxKernelChannelCombineCreate(
                     (dst->valid_roi.end_y - dst->valid_roi.start_y)/2;
                 vxlib_dst1.stride_y =
                     dst->imagepatch_addr[1].stride_y;
-                vxlib_dst1.data_type = VXLIB_UINT8;
+                vxlib_dst1.data_type = (uint32_t)VXLIB_UINT8;
 
                 if(dst->format == (vx_df_image)VX_DF_IMAGE_NV21)
                 {
@@ -852,7 +852,7 @@ static vx_status VX_CALLBACK tivxKernelChannelCombineCreateInBamGraph(
                     vertSamplingFactor[BAM_VXLIB_CHANNELCOMBINE_2TO1_I8U_O8U_NUM_INPUT_BLOCKS\
                                           + BAM_VXLIB_CHANNELCOMBINE_2TO1_I8U_O8U_OUTPUT_PORT] = 0.5f;
                 kernel_details[*bam_node_cnt].kernel_info.kernelExtraInfo.\
-                    typeOutputElmt[BAM_VXLIB_CHANNELCOMBINE_2TO1_I8U_O8U_OUTPUT_PORT] = VXLIB_UINT8;
+                    typeOutputElmt[BAM_VXLIB_CHANNELCOMBINE_2TO1_I8U_O8U_OUTPUT_PORT] = (uint32_t)VXLIB_UINT8;
 
                 if( dst->format == (vx_df_image)VX_DF_IMAGE_NV12)
                 {

kernels/openvx-core/c66x/bam/vx_bam_channel_extract_target.c
@@ -424,7 +424,7 @@ static vx_status tivxBamChannelExtractRgbRgbxInput(
 
         if(src->format == (vx_df_image)VX_DF_IMAGE_RGB)
         {
-            vxlib_src.data_type = VXLIB_UINT24;
+            vxlib_src.data_type = (uint32_t)VXLIB_UINT24;
 
             BAM_VXLIB_channelExtract_1of3_i8u_o8u_params params;
 
@@ -441,7 +441,7 @@ static vx_status tivxBamChannelExtractRgbRgbxInput(
         }
         else if(src->format == (vx_df_image)VX_DF_IMAGE_RGBX)
         {
-            vxlib_src.data_type = VXLIB_UINT32;
+            vxlib_src.data_type = (uint32_t)VXLIB_UINT32;
 
             BAM_VXLIB_channelExtract_1of4_i8u_o8u_params params;
 

kernels/openvx-core/c66x/bam/vx_bam_color_convert_target.c
@@ -355,10 +355,10 @@ static vx_status VX_CALLBACK tivxKernelBamColorConvertCreate(
             if (((vx_df_image)VX_DF_IMAGE_RGB == src->format) &&
                 ((vx_df_image)VX_DF_IMAGE_YUV4 == dst->format))
             {
-                vxlib_src[0].data_type = VXLIB_UINT24;
-                vxlib_dst[0].data_type = VXLIB_UINT8;
-                vxlib_dst[1].data_type = VXLIB_UINT8;
-                vxlib_dst[2].data_type = VXLIB_UINT8;
+                vxlib_src[0].data_type = (uint32_t)VXLIB_UINT24;
+                vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT8;
+                vxlib_dst[1].data_type = (uint32_t)VXLIB_UINT8;
+                vxlib_dst[2].data_type = (uint32_t)VXLIB_UINT8;
 
                 buf_params[0] = &vxlib_src[0];
                 buf_params[1] = &vxlib_dst[0];
@@ -375,9 +375,9 @@ static vx_status VX_CALLBACK tivxKernelBamColorConvertCreate(
             else if (((vx_df_image)VX_DF_IMAGE_RGB == src->format) &&
                      ((vx_df_image)VX_DF_IMAGE_NV12 == dst->format))
             {
-                vxlib_src[0].data_type = VXLIB_UINT24;
-                vxlib_dst[0].data_type = VXLIB_UINT8;
-                vxlib_dst[1].data_type = VXLIB_UINT8;
+                vxlib_src[0].data_type = (uint32_t)VXLIB_UINT24;
+                vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT8;
+                vxlib_dst[1].data_type = (uint32_t)VXLIB_UINT8;
 
                 buf_params[0] = &vxlib_src[0];
                 buf_params[1] = &vxlib_dst[0];
@@ -393,10 +393,10 @@ static vx_status VX_CALLBACK tivxKernelBamColorConvertCreate(
             else if (((vx_df_image)VX_DF_IMAGE_RGB == src->format) &&
                      ((vx_df_image)VX_DF_IMAGE_IYUV == dst->format))
             {
-                vxlib_src[0].data_type = VXLIB_UINT24;
-                vxlib_dst[0].data_type = VXLIB_UINT8;
-                vxlib_dst[1].data_type = VXLIB_UINT8;
-                vxlib_dst[2].data_type = VXLIB_UINT8;
+                vxlib_src[0].data_type = (uint32_t)VXLIB_UINT24;
+                vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT8;
+                vxlib_dst[1].data_type = (uint32_t)VXLIB_UINT8;
+                vxlib_dst[2].data_type = (uint32_t)VXLIB_UINT8;
 
                 buf_params[0] = &vxlib_src[0];
                 buf_params[1] = &vxlib_dst[0];
@@ -413,8 +413,8 @@ static vx_status VX_CALLBACK tivxKernelBamColorConvertCreate(
             else if (((vx_df_image)VX_DF_IMAGE_RGB == src->format) &&
                      ((vx_df_image)VX_DF_IMAGE_RGBX == dst->format))
             {
-                vxlib_src[0].data_type = VXLIB_UINT24;
-                vxlib_dst[0].data_type = VXLIB_UINT32;
+                vxlib_src[0].data_type = (uint32_t)VXLIB_UINT24;
+                vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT32;
 
                 buf_params[0] = &vxlib_src[0];
                 buf_params[1] = &vxlib_dst[0];
@@ -429,8 +429,8 @@ static vx_status VX_CALLBACK tivxKernelBamColorConvertCreate(
             else if (((vx_df_image)VX_DF_IMAGE_RGBX == src->format) &&
                      ((vx_df_image)VX_DF_IMAGE_RGB == dst->format))
             {
-                vxlib_src[0].data_type = VXLIB_UINT32;
-                vxlib_dst[0].data_type = VXLIB_UINT24;
+                vxlib_src[0].data_type = (uint32_t)VXLIB_UINT32;
+                vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT24;
 
                 buf_params[0] = &vxlib_src[0];
                 buf_params[1] = &vxlib_dst[0];
@@ -445,10 +445,10 @@ static vx_status VX_CALLBACK tivxKernelBamColorConvertCreate(
             else if (((vx_df_image)VX_DF_IMAGE_RGBX == src->format) &&
                      ((vx_df_image)VX_DF_IMAGE_YUV4 == dst->format))
             {
-                vxlib_src[0].data_type = VXLIB_UINT32;
-                vxlib_dst[0].data_type = VXLIB_UINT8;
-                vxlib_dst[1].data_type = VXLIB_UINT8;
-                vxlib_dst[2].data_type = VXLIB_UINT8;
+                vxlib_src[0].data_type = (uint32_t)VXLIB_UINT32;
+                vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT8;
+                vxlib_dst[1].data_type = (uint32_t)VXLIB_UINT8;
+                vxlib_dst[2].data_type = (uint32_t)VXLIB_UINT8;
 
                 buf_params[0] = &vxlib_src[0];
                 buf_params[1] = &vxlib_dst[0];
@@ -465,9 +465,9 @@ static vx_status VX_CALLBACK tivxKernelBamColorConvertCreate(
             else if (((vx_df_image)VX_DF_IMAGE_RGBX == src->format) &&
                      ((vx_df_image)VX_DF_IMAGE_NV12 == dst->format))
             {
-                vxlib_src[0].data_type = VXLIB_UINT32;
-                vxlib_dst[0].data_type = VXLIB_UINT8;
-                vxlib_dst[1].data_type = VXLIB_UINT8;
+                vxlib_src[0].data_type = (uint32_t)VXLIB_UINT32;
+                vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT8;
+                vxlib_dst[1].data_type = (uint32_t)VXLIB_UINT8;
 
                 buf_params[0] = &vxlib_src[0];
                 buf_params[1] = &vxlib_dst[0];
@@ -483,10 +483,10 @@ static vx_status VX_CALLBACK tivxKernelBamColorConvertCreate(
             else if (((vx_df_image)VX_DF_IMAGE_RGBX == src->format) &&
                      ((vx_df_image)VX_DF_IMAGE_IYUV == dst->format))
             {
-                vxlib_src[0].data_type = VXLIB_UINT32;
-                vxlib_dst[0].data_type = VXLIB_UINT8;
-                vxlib_dst[1].data_type = VXLIB_UINT8;
-                vxlib_dst[2].data_type = VXLIB_UINT8;
+                vxlib_src[0].data_type = (uint32_t)VXLIB_UINT32;
+                vxlib_dst[0].data_type = (uint32_t)VXLIB_UINT8;
+                vxlib_dst[1].data_type = (uint32_t)VXLIB_UINT8;
+                vxlib_dst[2].data_type = (uint32_t)VXLIB_UINT8;
 
                 buf_params[0] = &vxlib_src[0];
                 buf_params[1] = &vxlib_dst[0];

commit 020f0c2553f9ce87b4126d6026b6821791c32780

[TIOVX-56] MISRAC: Fix compile bugs added in prior patch
- For BAM enabled, there were a few bugs introduced in:
  af0dbb22d90a7895167d27d2aa75b3e7ceef3500 and
  20c854bd6aa210ce63b8eb9cc64c66f93944ead3

kernels/openvx-core/c66x/bam/vx_bam_harris_corners_target.c
@@ -186,7 +186,7 @@ static vx_status VX_CALLBACK tivxKernelHarrisCornersProcess(
         void *img_ptrs[2];
         void *src_target_ptr;
         void *arr_target_ptr;
-        VXLIB_STATUS status_vxlib = (vx_status)VXLIB_SUCCESS;
+        VXLIB_STATUS status_vxlib = (VXLIB_STATUS)VXLIB_SUCCESS;
 
         src_target_ptr = tivxMemShared2TargetPtr(&src->mem_ptr[0]);
         arr_target_ptr = tivxMemShared2TargetPtr(&arr->mem_ptr);

kernels/openvx-core/c66x/bam/vx_bam_intgimg_target.c
@@ -205,7 +205,7 @@ static vx_status VX_CALLBACK tivxKernelIntgImgCreate(
             BAM_VXLIB_integralImage_i8u_o32u_getKernelInfo( &kernel_params,
                                                         &kernel_details.kernel_info);
 
-            status = tivxBamCreateHandleSingleNode(BAM_KERNELID_(uint32_t)VXLIB_INTEGRALIMAGE_I8U_O32U,
+            status = tivxBamCreateHandleSingleNode(BAM_KERNELID_VXLIB_INTEGRALIMAGE_I8U_O32U,
                                                    buf_params, &kernel_details,
                                                    &prms->graph_handle);
         }
@@ -342,7 +342,7 @@ static vx_status VX_CALLBACK tivxKernelIntgImgCreateInBamGraph(
             memset(prms, 0, sizeof(tivxIntgImgParams));
 
             node_list[*bam_node_cnt].nodeIndex = *bam_node_cnt;
-            node_list[*bam_node_cnt].kernelId = BAM_KERNELID_(uint32_t)VXLIB_INTEGRALIMAGE_I8U_O32U;
+            node_list[*bam_node_cnt].kernelId = BAM_KERNELID_VXLIB_INTEGRALIMAGE_I8U_O32U;
             node_list[*bam_node_cnt].kernelArgs = NULL;
 
             kernel_params->frameWidth   = dst->valid_roi.end_x - dst->valid_roi.start_x;
@@ -394,11 +394,11 @@ static vx_status VX_CALLBACK tivxKernelIntgImgGetNodePort(
         {
             case TIVX_KERNEL_INTG_IMG_INPUT_IDX:
                 *bam_node = prms->bam_node_num;
-                *bam_port = BAM_(uint32_t)VXLIB_INTEGRALIMAGE_I8U_O32U_INPUT_IMAGE_PORT;
+                *bam_port = BAM_VXLIB_INTEGRALIMAGE_I8U_O32U_INPUT_IMAGE_PORT;
                 break;
             case TIVX_KERNEL_INTG_IMG_OUTPUT_IDX:
                 *bam_node = prms->bam_node_num;
-                *bam_port = BAM_(uint32_t)VXLIB_INTEGRALIMAGE_I8U_O32U_OUTPUT_IMAGE_PORT;
+                *bam_port = BAM_VXLIB_INTEGRALIMAGE_I8U_O32U_OUTPUT_IMAGE_PORT;
                 break;
             default:
                 VX_PRINT(VX_ZONE_ERROR,"tivxKernelIntgImgGetNodePort: non existing index queried by tivxKernelSupernodeCreate.tivxGetNodePort()\n");