0b235cbb1ae359fb4d2211eb75cb58fe4b471fae
Lucas Weaver <l-weaver@ti.com>
2020-08-17 11:19:24 -0500

    [CONFIG] Adding more descriptive logs for max context references

--- source/framework/vx_context.c
+++ source/framework/vx_context.c
@@ -412,6 +412,12 @@ vx_bool ownAddReferenceToContext(vx_context context, vx_reference ref)
             }
         }
 
+        if ((vx_bool)vx_false_e == is_success)
+        {
+            VX_PRINT(VX_ZONE_ERROR, "Max context references exceeded\n");
+            VX_PRINT(VX_ZONE_ERROR, "May need to increase the value of TIVX_CONTEXT_MAX_REFERENCES in tiovx/include/TI/tivx_config.h\n");
+        }
+
         ownContextUnlock(context);
     }
     return is_success;
