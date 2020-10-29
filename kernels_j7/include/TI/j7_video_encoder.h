8dd63d340bb955e6d6aef8ef3a353e48e40cc94f
David Huang <d-huang@ti.com>
2020-10-29 17:11:59 -0500

    Video Encoder: Add documentation to TI encoder node
    Signed-off-by: David Huang <d-huang@ti.com>

--- kernels_j7/include/TI/j7_video_encoder.h
+++ kernels_j7/include/TI/j7_video_encoder.h
@@ -102,23 +102,39 @@ extern "C" {
  *********************************/
 
 /**
- * \verbatim
- *  MM Encoder supported features
- *  Application shall use this type to configure encoder features \endverbatim
+ *  MM Encoder supported features \n
+ *  Application shall use this type to configure encoder features
  */
 typedef enum {
-	TIVX_ENC_FEATURE_CABAC = 0x01,
-	TIVX_ENC_FEATURE_8x8 = 0x02
+    /**
+     *  Enable CABAC encoding. Forces a minimum of MAIN PROFILE.
+     */
+    TIVX_ENC_FEATURE_CABAC = 0x01,
+    /**
+     *  Enable 8x8 transform. Forces a minimum of HIGH PROFILE.
+     */
+    TIVX_ENC_FEATURE_8x8 = 0x02
 } tivx_enc_features_e;
 
 /**
- * \verbatim
- *  MM Encoder supported RC Modes
- *  Application shall use this type to configure encoder RC mode \endverbatim
+ *  MM Encoder supported RC Modes \n
+ *  Application shall use this type to configure encoder RC mode
  */
 typedef enum {
-	TIVX_ENC_VBR,
-	TIVX_ENC_SVBR
+    /**
+     *  Use Variable Bit Rate (VBR) rate control. \n
+     *  This allows different bits allocated per frame, meeting the overall
+     *  bitrate requirement over a sliding window.
+     */
+    TIVX_ENC_VBR,
+    /**
+     *  Use Streaming Variable Bit Rate (SVBR) rate control. \n
+     *  This is similar to VBR, but due to disabling bit stuffing a constant
+     *  output bitrate is not guaranteed. However, this does still guarantee
+     *  that a decoder fed at the specified bitrate will be able to decode
+     *  successfully.
+     */
+    TIVX_ENC_SVBR
 } tivx_enc_rcmode_e;
 
 
@@ -130,33 +146,113 @@ typedef enum {
  * \ingroup group_vision_function_video_encoder
  */
 typedef struct {
+    /**
+     *  Bitstream format for encoded output. Valid values: \n
+     *    TIVX_BITSTREAM_FORMAT_H264
+     */
     uint32_t bitstream_format;
-    /** Bit flags for encoding features, see tivx_enc_features_e */
+    /**
+     *  Bit flags for encoding features, see \ref tivx_enc_features_e \n
+     *  Note that this is used to control H264 profile as well, with profile set
+     *  to the minimum required for the features enabled.
+     */
     uint32_t features;
     /** RC Mode, see \ref tivx_enc_rcmode_e */
     uint32_t rcmode;
-    /** IDR-period */
+    /**
+     *  IDR period is the number of frames between Instantaneous Decode Refresh
+     *  (IDR) frames. IDR frames guarantee that no frames after the IDR frame
+     *  will reference any frames prior to the IDR frame. \n
+     *  A typical value can be around 1-2 seconds worth of frames \n
+     *    ex. at 30fps, idr_period = 30 for 1 second, idr_period = 60 for 2 seconds
+     */
     uint32_t idr_period;
-    /** I-period */
+    /**
+     *  I period is the number of frames between Intra frames (I-frames).
+     *  I-frames are frames encoded with all of the image data for the entire
+     *  frame, without needing reference to any other frames. \n
+     *  Setting any I-frame period higher than one fills the frames in between
+     *  with Predicted frames (P-frames). P-frames can reference data in frames
+     *  prior to themselves, so rely on prior decoded frames for decoding. \n
+     *    ex. i_period = 30 means there will be a pattern of one I-frame followed by 29 P-frames, then repeat. \n
+     *  Setting i_period = 1 sets up I-frame only encoding.
+     */
     uint32_t i_period;
-    /** Bitrate */
+    /**
+     *  Bitrate is the number of bits per second of encoding. This is set in bps, not kbps or mbps. \n
+     *  For a 1920x1080 VBR stream, a bitrate of around 10000000 is normal.
+     */
     uint32_t bitrate;
-    /** Framerate */
+    /**
+     *  Framerate sets how many frames per second the stream is encoded for.
+     *  This primarily impacts the bits the RC control will try to allocate per
+     *  frame based on this and the bitrate. \n
+     *  Typical framerates are 30 and 60 fps.
+     */
     uint32_t framerate;
-    /** Crop settings */
+    /**
+     *  Crop settings set how much to crop off of each side from the original image. \n\n
+     *  NOTE: Width and Height of the video buffers must be 16-aligned to work with the driver.
+     *    By default if a non-16-aligned value is input for buffer width/height, the driver will round down to 16-align.
+     *    In order to get a full width/height from a non-16-aligned stream size the application must read the data into the buffer
+     *    starting at the "upper left" corner, making sure lines are width aligned. Then an appropriate crop must be set for the
+     *    encoder to crop out the empty data along the edges. \n\n
+     *  ex. 1920x1080 \n
+     *    Set to 1920x1088 width/height. Upon filling the input data this will leave 8 lines of empty data in the bottom of the buffer. \n
+     *    Set crop_bottom = 8 to crop the empty 8 lines at the bottom of the buffer to avoid green lines. \n\n
+     *  NOTE: crop values can only be set in multiples of 2. Any odd numbered crop will be rounded down in the driver. \n
+     *  These should be set to 0 by default.
+     */
     uint32_t crop_left;
+    /**  See \ref crop_left */
     uint32_t crop_right;
+    /**  See \ref crop_left */
     uint32_t crop_top;
+    /**  See \ref crop_left */
     uint32_t crop_bottom;
-    /** # Slices */
+    /**
+     *  The number of slices to divide each frame into for encoding.
+     *  Under normal circumstances, this should be set to 1. \n
+     *  This can be set to 2 to take advantage of the encoder's 2 hardware pipes
+     *  for lower latency encoding, but this latency advantage is only evident
+     *  if this is the only stream being encoded.
+     */
     uint32_t nslices;
-    /** base pipe */
+    /**
+     *  Sets which base pipe to use within the encoder. Valid values are 0 and 1. \n
+     *  The encoder has 2 hardware encoding pipes. For best performance,
+     *  multiple streams should be split among both pipes to minimize time spent
+     *  waiting for a different stream to finish encoding a frame.\n\n
+     *  For 2-slice encoding targeting minimal latency per frame, this should be set to 0 to allow it to use both pipe 0 and pipe 1.
+     */
     uint32_t base_pipe;
-    /** Qp Settings */
+    /**
+     *  Qp Settings set the Quantization Parameters (QP). By default leaving
+     *  these at 0 will allow the driver to automatically calculate appropriate
+     *  settings based on the stream. \n
+     *  These settings impact how the encoder targets encoding for more static or dynamic streams. \n
+     *  A lower QP value allows for a high quality encoding but less compression,
+     *  while a higher QP value allows for greater compression but lower quality encoding. \n
+     *  If QP is too low for the amount of motion in the stream, this can produce
+     *  artifacts as the encoder runs out of bits for frame data at the quality
+     *  that QP is specifying. \n
+     *  As a stream is encoded, QP values will be automatically adjusted to
+     *  values between mix_qp and max_qp based on the stream contents, attempting
+     *  to maximize quality for a given stream. \n\n
+     *
+     *  Initial QP settings (initial_qp_i/p/b) set the initial QP values for I/P/B frames respectively.
+     *  This impacts the stream quality at the start of the stream, before the encoder has had a chance to adapt to the stream. \n\n
+     *
+     *  min_qp/max_qp set the minimum and maximum QP values respectively. Values are between 2 and 49.
+     */
     uint32_t initial_qp_i;
+    /** See \ref initial_qp_i */
     uint32_t initial_qp_p;
+    /** See \ref initial_qp_i */
     uint32_t initial_qp_b;
+    /** See \ref initial_qp_i */
     uint32_t min_qp;
+    /** See \ref initial_qp_i */
     uint32_t max_qp;
 } tivx_video_encoder_params_t;
 
