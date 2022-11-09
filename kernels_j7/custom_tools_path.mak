TexasInstruments/tiovx

commit cd9ccaeff9256ae035b57e094d1f4d0fdf6c7355

[AM62A] Add support for AM62A and VPAC3L
JIRA: Satisfies TIOVX-1150, TIOVX-1148

AM62A HW-specific deltas from other platforms:
----------------------------------------------
- Added target configuration matching supported cores on AM62A
- Added new C7x variant support : C7504
- Added explicit cache maintenance operation in TIDL node on C7x
- Added explicit cache maintenance operation on A-core
- VPAC drivers from MCU1_0 instead of MCU2_0
- Disabled DMPAC nodes for AM62A
- Deltas to support VPAC3L instead of VPAC or VPAC3
- Removed VISS lastH3aInSrc functionality
- Removed VISS register configuration via DMA

AM62A SW-only deltas from other platforms (limited by requirements):
--------------------------------------------------------------------
- PC emulation mode disabled
- Single A-core target instead of 4 (nested graphs not supported)
- Disable C7x preemption

Additional updates:
-------------------
- Added per HWA build options (VISS, LDC, etc)

Signed-off-by: Shubham Jain <a0492788@ti.com>

kernels_j7/custom_tools_path.mak
@@ -4,5 +4,27 @@
 # and vsdk_tools_path.mak files from the tiovx directory.
 
 # set values of below to yes or no to include or exclude the modules from compile and link
-BUILD_HWA_DMPAC_DOF?=yes
-BUILD_HWA_KERNELS?=yes
+
+ifeq ($(SOC),am62a)
+	BUILD_HWA_KERNELS=yes
+	BUILD_CAPTURE=no
+	BUILD_CSITX=no
+	BUILD_DISPLAY=no
+	BUILD_DMPAC_DOF?=no
+	BUILD_DMPAC_SDE=no
+	BUILD_VPAC_VISS=yes
+	BUILD_VPAC_MSC=yes
+	BUILD_VPAC_LDC=yes
+	BUILD_VPAC_NF=no
+else
+	BUILD_HWA_KERNELS=yes
+	BUILD_CAPTURE=yes
+	BUILD_CSITX=yes
+	BUILD_DISPLAY=yes
+	BUILD_DMPAC_DOF?=yes
+	BUILD_DMPAC_SDE=yes
+	BUILD_VPAC_VISS=yes
+	BUILD_VPAC_MSC=yes
+	BUILD_VPAC_LDC=yes
+	BUILD_VPAC_NF=yes
+endif

commit 5068d2d38faf1a51f51e59c30b98571db7aa74f1

[BUILD] Enable default HWA module BUILD flags to be set from env
- Bug introduced in cd9ccaeff9256ae035b57e094d1f4d0fdf6c7355
  - This commit removed the '?' from BUILD_HWA_KERNELS?=yes
- This was breaking release build flow which was dependent on
  setting BUILD_HWA_KERNELS from environment
- While I was at it, I also made the others settable from the
  environment (except in case of AM62A).

kernels_j7/custom_tools_path.mak
@@ -5,26 +5,23 @@
 
 # set values of below to yes or no to include or exclude the modules from compile and link
 
+BUILD_HWA_KERNELS?=yes
+BUILD_VPAC_VISS?=yes
+BUILD_VPAC_MSC?=yes
+BUILD_VPAC_LDC?=yes
+
 ifeq ($(SOC),am62a)
-	BUILD_HWA_KERNELS=yes
 	BUILD_CAPTURE=no
 	BUILD_CSITX=no
 	BUILD_DISPLAY=no
-	BUILD_DMPAC_DOF?=no
+	BUILD_DMPAC_DOF=no
 	BUILD_DMPAC_SDE=no
-	BUILD_VPAC_VISS=yes
-	BUILD_VPAC_MSC=yes
-	BUILD_VPAC_LDC=yes
 	BUILD_VPAC_NF=no
 else
-	BUILD_HWA_KERNELS=yes
-	BUILD_CAPTURE=yes
-	BUILD_CSITX=yes
-	BUILD_DISPLAY=yes
+	BUILD_CAPTURE?=yes
+	BUILD_CSITX?=yes
+	BUILD_DISPLAY?=yes
 	BUILD_DMPAC_DOF?=yes
-	BUILD_DMPAC_SDE=yes
-	BUILD_VPAC_VISS=yes
-	BUILD_VPAC_MSC=yes
-	BUILD_VPAC_LDC=yes
-	BUILD_VPAC_NF=yes
+	BUILD_DMPAC_SDE?=yes
+	BUILD_VPAC_NF?=yes
 endif