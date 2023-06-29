650db666d03c6eee90f385735a8eb08ab38fda6f
Brock Carey <b-carey@ti.com>
2023-06-29 14:36:05 -0500

    [CONFIG] Increased values of platform object descriptor instances

--- source/platform/pc/common/tivx_platform_pc.h
+++ source/platform/pc/common/tivx_platform_pc.h
@@ -92,7 +92,7 @@ extern "C" {
 /*! \brief Maximum number obj descriptors that are present in shared memory
  * \ingroup group_tivx_platform
  */
-#define TIVX_PLATFORM_MAX_OBJ_DESC_SHM_INST  (1024U)
+#define TIVX_PLATFORM_MAX_OBJ_DESC_SHM_INST  (2048u)
 
 
 #if defined (SOC_J721E)
