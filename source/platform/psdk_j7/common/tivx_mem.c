ece1c70cf484c0a362d33c3a7090f8ea0c6a5f88
Lucas Weaver <l-weaver@ti.com>
2023-09-07 11:01:27 -0500

    [TIOVX-1360] Removing cache maintenance operations for J784S4 on A72

--- source/platform/psdk_j7/common/tivx_mem.c
+++ source/platform/psdk_j7/common/tivx_mem.c
@@ -328,7 +328,7 @@ vx_status tivxMemBufferMap(
     {
         if ((vx_enum)TIVX_MEMORY_TYPE_DMA != mem_type)
         {
-            #if defined(SOC_AM62A) || (defined(SOC_J784S4) && !(defined(A72)))
+            #if defined(SOC_AM62A)
             appMemCacheInv(host_ptr, size);
             #else
             #ifndef A72
@@ -356,7 +356,7 @@ vx_status tivxMemBufferUnmap(
         if (((vx_enum)TIVX_MEMORY_TYPE_DMA != mem_type) &&
             (((vx_enum)VX_WRITE_ONLY == maptype) || ((vx_enum)VX_READ_AND_WRITE == maptype)))
         {
-            #if defined(SOC_AM62A) || defined(SOC_J784S4)
+            #if defined(SOC_AM62A)
             appMemCacheWb(host_ptr, size);
             #else
             #ifndef A72
