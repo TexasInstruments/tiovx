3adb383f0515254ded27ebcff91e918b547775bd
Vishal Roy <x1120960@ti.com>
2023-11-22 14:53:01 +0530

    [TEST][OBJECT] -Added testcase to cover -ve conditions of ObjecDeInit() -Added testcases to cover ObjectFree
    [TEST][OBJECTS]
    -Remove Vscode unwanted files
    
    [TEST][OBJECTS]
    -Addition of comments.

--- conformance_tests/test_internal/test_objects.c
+++ conformance_tests/test_internal/test_objects.c
@@ -74,7 +74,72 @@ TEST_WITH_ARG(tivxInternalObjects, negativeTestObjectDeInit, Arg,
         VX_CALL(ownObjectInit());
     }
 }
+TEST_WITH_ARG(tivxInternalObjects, negativeTestObjectDeInit1, Arg,
+              ARG_ENUM(VX_TYPE_META_FORMAT),
+              ARG_ENUM(VX_TYPE_CONTEXT),
+              ARG_ENUM(VX_TYPE_GRAPH),
+              ARG_ENUM(VX_TYPE_NODE),
+              ARG_ENUM(VX_TYPE_KERNEL),
+              ARG_ENUM(VX_TYPE_ARRAY),
+              ARG_ENUM(VX_TYPE_USER_DATA_OBJECT),
+              ARG_ENUM(TIVX_TYPE_RAW_IMAGE),
+              ARG_ENUM(VX_TYPE_CONVOLUTION),
+              ARG_ENUM(VX_TYPE_DELAY),
+              ARG_ENUM(VX_TYPE_DISTRIBUTION),
+              ARG_ENUM(VX_TYPE_IMAGE),
+              ARG_ENUM(VX_TYPE_TENSOR),
+              ARG_ENUM(VX_TYPE_LUT),
+              ARG_ENUM(VX_TYPE_MATRIX),
+              ARG_ENUM(VX_TYPE_PYRAMID),
+              ARG_ENUM(VX_TYPE_REMAP),
+              ARG_ENUM(VX_TYPE_SCALAR),
+              ARG_ENUM(VX_TYPE_THRESHOLD),
+              ARG_ENUM(VX_TYPE_ERROR),
+              ARG_ENUM(VX_TYPE_OBJECT_ARRAY),
+              ARG_ENUM(VX_TYPE_PARAMETER))
+{
+    static tivx_object_t g_tivx_objects;
+    vx_reference ref = NULL;
+    type = (enum vx_type_e)arg_->type;
 
+    ref = ownObjectAlloc(type);
+    EXPECT(ref != NULL);
+    if (NULL != ref)
+    {
+        ref->type = type;
+        //Trigger -ve condition when de-initializing a ref object
+        ASSERT_EQ_VX_STATUS(VX_ZONE_ERROR, ownObjectDeInit());
+        VX_CALL(ownObjectInit());
+    }
+}
+TEST(tivxInternalObjects, negativeTestObjectAlloc)
+{
+    vx_reference ref = NULL;
+
+    ref = ownObjectAlloc(VX_TYPE_REFERENCE);
+    EXPECT(ref == NULL);
+}
+TEST(tivxInternalObjects, negativeTestObjectFree)
+{
+    vx_context context = context_->vx_context_;
+    vx_reference ref = NULL;
+    vx_status status = VX_SUCCESS;
+    //VX_TYPE_LUT element was used to create a error condition any other element 
+    //can be used here provided its not part of the switch case of ownObjectAlloc
+    ref = ownObjectAlloc(VX_TYPE_LUT);
+    EXPECT(ref != NULL);
+
+    ref->type = VX_TYPE_REFERENCE;
+    status = ownObjectFree(ref);
+    EXPECT(status == VX_ERROR_INVALID_REFERENCE);
+
+    ref->type = VX_TYPE_LUT;
+    status = ownObjectFree(ref);
+    EXPECT(status == VX_SUCCESS);
+}
 TESTCASE_TESTS(
     tivxInternalObjects,
-    negativeTestObjectDeInit)
+    negativeTestObjectDeInit,
+    negativeTestObjectAlloc,
+    negativeTestObjectFree,
+    negativeTestObjectDeInit1)
