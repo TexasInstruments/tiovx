12edeb7c6d6b8ecc50b81e2b6a780bb738b19daa
Shilpa John <x1120129@ti.com>
2024-02-16 19:01:33 +0530

    [TEST][OBJECTS] -Added negative test case for ownObjectDeInit for hitting the ownCheckUseFlag of Data Ref Queue. Added test cases for reference Null in ownObjectFree for branch coverage and negative test cases for checking while the data ref queue is freed in ownObjectFree for test coverage. Added testcases for deleting the mutex and thus making g_tivx_objects_lock Null in ownObjectDeInit, ownObjectAlloc, ownObjectFree

--- conformance_tests/test_internal/test_objects.c
+++ conformance_tests/test_internal/test_objects.c
@@ -55,7 +55,8 @@ TEST_WITH_ARG(tivxInternalObjects, negativeTestObjectDeInit, Arg,
               ARG_ENUM(VX_TYPE_THRESHOLD),
               ARG_ENUM(VX_TYPE_ERROR),
               ARG_ENUM(VX_TYPE_OBJECT_ARRAY),
-              ARG_ENUM(VX_TYPE_PARAMETER))
+              ARG_ENUM(VX_TYPE_PARAMETER),
+              ARG_ENUM(TIVX_TYPE_DATA_REF_Q))
 {
     static tivx_object_t g_tivx_objects;
     vx_reference ref = NULL;
@@ -112,18 +113,44 @@ TEST_WITH_ARG(tivxInternalObjects, negativeTestObjectDeInit1, Arg,
         VX_CALL(ownObjectInit());
     }
 }
+TEST(tivxInternalObjects, negativeTestObjectDeInit2)
+{
+    static tivx_object_t g_tivx_objects;
+    vx_reference ref = NULL,ref1 = NULL;
+    ref = ownObjectAlloc(TIVX_TYPE_DATA_REF_Q);
+    EXPECT(ref != NULL);
+    if (NULL != ref)
+    {
+        ASSERT_EQ_VX_STATUS(VX_FAILURE, ownObjectDeInit());
+        VX_CALL(ownObjectInit());
+    }
+
+    ref1 = ownObjectAlloc(VX_TYPE_META_FORMAT);
+    EXPECT(ref != NULL);
+    VX_CALL(ownObjectDeInit());
+    ASSERT_EQ_VX_STATUS(VX_FAILURE, ownObjectDeInit());
+    VX_CALL(ownObjectInit());
+}
 TEST(tivxInternalObjects, negativeTestObjectAlloc)
 {
     vx_reference ref = NULL;
 
     ref = ownObjectAlloc(VX_TYPE_REFERENCE);
     EXPECT(ref == NULL);
+
+    VX_CALL(ownObjectDeInit());
+    ref = ownObjectAlloc(VX_TYPE_REFERENCE);
+    EXPECT(ref == NULL);
+    VX_CALL(ownObjectInit());
 }
 TEST(tivxInternalObjects, negativeTestObjectFree)
 {
     vx_context context = context_->vx_context_;
     vx_reference ref = NULL;
     vx_status status = VX_SUCCESS;
+
+    status = ownObjectFree(ref);
+    EXPECT(status == VX_FAILURE);
     //VX_TYPE_LUT element was used to create a error condition any other element 
     //can be used here provided its not part of the switch case of ownObjectAlloc
     ref = ownObjectAlloc(VX_TYPE_LUT);
@@ -136,10 +163,21 @@ TEST(tivxInternalObjects, negativeTestObjectFree)
     ref->type = VX_TYPE_LUT;
     status = ownObjectFree(ref);
     EXPECT(status == VX_SUCCESS);
+
+    ref = ownObjectAlloc(VX_TYPE_LUT);
+    VX_CALL(ownObjectDeInit());
+    status = ownObjectFree(ref);
+    EXPECT(status == VX_ERROR_INVALID_PARAMETERS);
+    VX_CALL(ownObjectInit());
+    ref = ownObjectAlloc(VX_TYPE_LUT);
+    status = ownObjectFree(ref);
+    EXPECT(status == VX_SUCCESS);
+
 }
 TESTCASE_TESTS(
     tivxInternalObjects,
     negativeTestObjectDeInit,
     negativeTestObjectAlloc,
     negativeTestObjectFree,
-    negativeTestObjectDeInit1)
+    negativeTestObjectDeInit1,
+    negativeTestObjectDeInit2)
