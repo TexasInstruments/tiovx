2a0efc94ad390c6443519307371a2814d0db3472
Lucas Weaver <l-weaver@ti.com>
2024-03-18 13:27:12 -0500

    [TIOVX-1314] Adding performance documentation for 9.2
    -Also adding J722S performance information for the first time

---
+++ internal_docs/doxy_cfg_user_guide/content_pages_j7/j722s/performance.h
@@ -0,0 +1,36 @@
+/*!
+    \page TIOVX_PERFORMANCE Datasheet
+
+    <TABLE frame="box" rules="all" cellspacing="0" width="50%" border="1" cellpadding="3">
+        <TR>
+            <TD> \subpage TIOVX_PERFORMANCE_J722S_LINUX </TD>
+        </TR>
+    </TABLE>
+ */
+
+ /*!
+    \page TIOVX_PERFORMANCE_J722S_LINUX J722S Linux Performance Report
+
+    <TABLE frame="box" rules="all" cellspacing="0" width="50%" border="1" cellpadding="3">
+        <TR>
+            <TD> <b> SoC </b></TD>
+            <TD> J722S </TD>
+        </TR>
+        <TR>
+            <TD> <b> Test EVM </b></TD>
+            <TD> J722S TI EVM </TD>
+        </TR>
+        <TR>
+            <TD> <b> HOST </b></TD>
+            <TD> ARM A53 running Linux at default frequency </TD>
+        </TR>
+        <TR>
+            <TD> <b> Target </b></TD>
+            <TD> DSP C7X running Free RTOS at default frequency, L1/L2 Cache Enabled (Cache-only based performance.)</TD>
+        </TR>
+    </TABLE>
+
+    <BR><HR><BR>
+
+    \htmlinclude tiovx_performance_J722S_LINUX.html
+ */
