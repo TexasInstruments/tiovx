TexasInstruments/tiovx

commit f6d0b603b37f07ca01cc22a2c2852a0f9d777642

[MISRA][PLATFORM][MISRA.FUNC.UNMATCHED.PARAMS]-Fixed misra warning by establishing match btween the function parameters.

include/TI/tivx_debug.h
@@ -72,7 +72,7 @@
 #define ASSERT_CONCAT_(a, b) a##b
 #define ASSERT_CONCAT(a, b) ASSERT_CONCAT_(a, b)
 #define BUILD_ASSERT(e) \
-     enum { ASSERT_CONCAT(assert_line_, __LINE__) = (bool)(1/(e)) }
+     enum { ASSERT_CONCAT(assert_line_, __LINE__) = (e) }
 
 /*!
  * \file

source/platform/psdk_j7/rtos/tivx_platform_rtos.c
@@ -59,8 +59,8 @@ vx_status ownPlatformInit(void)
     int32_t retVal;
 
     /* Build time check to see if the structure size is 8byte aligned and size of the elements is not more than  */
-    BUILD_ASSERT((((sizeof(tivx_obj_desc_shm_entry_t)) % (TIVX_PLATFORM_SHM_ENTRY_SIZE_ALIGN)) == 0U )? true : false);
-    BUILD_ASSERT(((sizeof(tivx_obj_desc_shm_entry_t)) <= (TIVX_OBJ_DESC_MAX_SHM_ENTRY_SIZE )) ? true : false);
+    BUILD_ASSERT((((sizeof(tivx_obj_desc_shm_entry_t)) % (TIVX_PLATFORM_SHM_ENTRY_SIZE_ALIGN)) == 0U )? 1U : 0U);
+    BUILD_ASSERT(((sizeof(tivx_obj_desc_shm_entry_t)) <= (TIVX_OBJ_DESC_MAX_SHM_ENTRY_SIZE )) ? 1U : 0U);
 
     retVal = appIpcGetTiovxObjDescSharedMemInfo( (void **) &gTivxObjDescShmEntry, &shmSize);
 

commit 441584e62f8dd58790ef8c1b5748707adee1d7e8

[TIOVX-1681] Bug fix to BUILD_ASSERT macro
- Bug introduced in f6d0b603
- The divide by zero is needed to trigger the build fail, removing
  the divide made this never fail at build, even when the value of
  e was set to 0.

include/TI/tivx_debug.h
@@ -72,7 +72,7 @@
 #define ASSERT_CONCAT_(a, b) a##b
 #define ASSERT_CONCAT(a, b) ASSERT_CONCAT_(a, b)
 #define BUILD_ASSERT(e) \
-     enum { ASSERT_CONCAT(assert_line_, __LINE__) = (e) }
+     enum { ASSERT_CONCAT(assert_line_, __LINE__) = (1/(e)) }
 
 /*!
  * \file