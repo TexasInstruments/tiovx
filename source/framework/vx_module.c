c3ce159d246951ddad662b6e60f02349fd3babf0
Shilpa John <x1120129@ti.com>
2024-09-13 12:07:15 +0530

    [UNTESTABLE][BRANCH][RATIONALE][FRAMEWORK][ARRAY] Tagged remaining untestable branches of vx_module.c
    * Marked TIOVX_BRANCH_COVERAGE_TIVX_MODULE_UBR001- TIOVX_BRANCH_COVERAGE_TIVX_MODULE_UBR004 as untestable branches.

--- source/framework/vx_module.c
+++ source/framework/vx_module.c
@@ -95,7 +95,7 @@ uint32_t ownGetModuleCount(void)
     {
         if( (g_module_table[idx].publish != NULL)
             &&
-            (g_module_table[idx].unpublish != NULL)
+            (g_module_table[idx].unpublish != NULL) /* TIOVX-1912- LDRA Uncovered Branch Id: TIOVX_BRANCH_COVERAGE_TIVX_MODULE_UBR001 */
             &&
             (g_module_table[idx].is_loaded != (vx_bool)vx_false_e)
           )
@@ -158,7 +158,7 @@ VX_API_ENTRY vx_status VX_API_CALL tivxUnRegisterModule(const char *name)
     {
         if( (g_module_table[idx].publish != NULL)
             &&
-            (g_module_table[idx].unpublish != NULL)
+            (g_module_table[idx].unpublish != NULL) /* TIOVX-1912- LDRA Uncovered Branch Id: TIOVX_BRANCH_COVERAGE_TIVX_MODULE_UBR002 */
             &&
             (strncmp(g_module_table[idx].name, name, TIVX_MODULE_MAX_NAME) == 0)
           )
@@ -189,7 +189,7 @@ VX_API_ENTRY vx_status VX_API_CALL vxLoadKernels(vx_context context, const vx_ch
     {
         if( (g_module_table[idx].publish != NULL)
             &&
-            (g_module_table[idx].unpublish != NULL)
+            (g_module_table[idx].unpublish != NULL)  /* TIOVX-1912- LDRA Uncovered Branch Id: TIOVX_BRANCH_COVERAGE_TIVX_MODULE_UBR003  */
             &&
             (strncmp(g_module_table[idx].name, module, TIVX_MODULE_MAX_NAME) == 0)
           )
@@ -234,7 +234,7 @@ VX_API_ENTRY vx_status VX_API_CALL vxUnloadKernels(vx_context context, const vx_
     {
         if( (g_module_table[idx].publish != NULL)
             &&
-            (g_module_table[idx].unpublish != NULL)
+            (g_module_table[idx].unpublish != NULL)  /* TIOVX-1912- LDRA Uncovered Branch Id: TIOVX_BRANCH_COVERAGE_TIVX_MODULE_UBR004  */
             &&
             (strncmp(g_module_table[idx].name, module, TIVX_MODULE_MAX_NAME) == 0)
             &&
