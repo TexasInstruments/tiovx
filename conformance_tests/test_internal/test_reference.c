273e4c85a40d93aac74b4065a9035646a5ba2f28
Vinuchandran <vinuchandran@ignitarium.com>
2024-09-17 17:07:26 +0530

    [TEST][FRAMEWORK][REFERENCE] Added 6 new tests to cover branches
    * Added the following tests to cover branches.
      - negativeTestOwnIsValidReference
      - negativeTestNullReference
      - negativeTestOwnReleaseReferenceInt
      - negativeTestOwnReferenceLock
      - negativeTestReleaseReference
      - negativeTestGetReferenceParent

--- conformance_tests/test_internal/test_reference.c
+++ conformance_tests/test_internal/test_reference.c
@@ -30,6 +30,8 @@
 #include <vx_internal.h>
 
 #include "shared_functions.h"
+#include "test_utils_mem_operations.h"
+
 
 TESTCASE(tivxInternalReference, CT_VXContext, ct_setup_vx_context, 0)
 
@@ -39,6 +41,100 @@ TEST(tivxInternalReference, negativeTestOwnDestructReferenceGeneric)
     ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_REFERENCE, ownDestructReferenceGeneric((vx_reference)context));
 }
 
+TEST(tivxInternalReference, negativeTestOwnIsValidReference)
+{
+    vx_context context = context_->vx_context_;
+    vx_image image = NULL;
+    vx_uint32 width = 16, height = 9;
+
+    ASSERT_VX_OBJECT(image = vxCreateImage(context, width, height, VX_DF_IMAGE_U8), VX_TYPE_IMAGE);
+    vx_enum ref_type = ((vx_reference)image)->type;
+    ((vx_reference)image)->type = VX_TYPE_INVALID;
+    ASSERT((vx_bool)vx_false_e == ownIsValidReference((vx_reference)image));
+    ((vx_reference)image)->type = ref_type;
+
+    vx_context base_context = context->base.context;
+    context->base.context = (vx_context)(vx_reference)image;
+    ASSERT((vx_bool)vx_false_e == ownIsValidReference((vx_reference)context));
+    context->base.context = base_context;
+
+    VX_CALL(vxReleaseImage(&image));
+}
+TEST(tivxInternalReference, negativeTestNullReference)
+{
+    vx_context context = context_->vx_context_;
+    ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_REFERENCE, ownCreateReferenceLock(NULL));
+    ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_REFERENCE, ownDeleteReferenceLock(NULL));
+    ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_REFERENCE, ownInitReference(NULL, NULL, 0, NULL));
+    ASSERT((vx_enum)TIVX_OBJ_DESC_INVALID == ownReferenceGetObjDescId(NULL));
+}
+
+TEST(tivxInternalReference, negativeTestOwnReleaseReferenceInt)
+{
+    vx_context context = context_->vx_context_;
+    vx_convolution conv;
+    vx_size rows = 3, cols = 3;
+
+    ASSERT_VX_OBJECT(conv = vxCreateConvolution(context, cols, rows), VX_TYPE_CONVOLUTION);
+
+    /* The Convolution is release without calling vxReleaseConvolution() by using the ownReleaseReferenceInt() function directly.
+    vxReleaseConvolution() need not be called again */
+    ownReleaseReferenceInt((vx_reference*)&conv, (vx_enum)VX_TYPE_CONVOLUTION, (vx_enum)VX_EXTERNAL, ((vx_reference)conv)->destructor_callback);
+}
+
+TEST(tivxInternalReference, negativeTestOwnReferenceLock)
+{
+    vx_context context = context_->vx_context_;
+    vx_image image = NULL;
+    vx_uint32 width = 16, height = 9;
+
+    ASSERT_VX_OBJECT(image = vxCreateImage(context, width, height, VX_DF_IMAGE_U8), VX_TYPE_IMAGE);
+
+    vx_context ref_context = ((vx_reference)image)->context;
+    ((vx_reference)image)->context = NULL;
+    ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_REFERENCE, ownReferenceLock((vx_reference)image));
+    ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_REFERENCE, ownReferenceUnlock((vx_reference)image));
+
+    ((vx_reference)image)->context = ref_context;
+
+    VX_CALL(vxReleaseImage(&image));
+
+}
+
+TEST(tivxInternalReference, negativeTestReleaseReference)
+{
+    vx_context context = context_->vx_context_;
+    vx_image image = NULL;
+    vx_uint32 width = 16, height = 9;
+
+    ASSERT_VX_OBJECT(image = vxCreateImage(context, width, height, VX_DF_IMAGE_U8), VX_TYPE_IMAGE);
+    tivx_reference_release_callback_f ref_callback = ((vx_reference)image)->release_callback;
+
+    ((vx_reference)image)->release_callback = NULL;
+
+    ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_REFERENCE, vxReleaseReference((vx_reference*)&image));
+    ((vx_reference)image)->release_callback = ref_callback;
+    VX_CALL(vxReleaseImage(&image));
+}
+
+TEST(tivxInternalReference, negativeTestGetReferenceParent)
+{
+    vx_context context = context_->vx_context_;
+    vx_image image = NULL;
+    vx_uint32 width = 16, height = 9;
+
+    ASSERT_VX_OBJECT(image = vxCreateImage(context, width, height, VX_DF_IMAGE_U8), VX_TYPE_IMAGE);
+    ASSERT(NULL == tivxGetReferenceParent((vx_reference) image));
+
+    VX_CALL(vxReleaseImage(&image));
+}
+
 TESTCASE_TESTS(tivxInternalReference,
-               negativeTestOwnDestructReferenceGeneric
+               negativeTestOwnDestructReferenceGeneric,
+               negativeTestOwnIsValidReference,
+               negativeTestNullReference,
+               negativeTestOwnReleaseReferenceInt,
+               negativeTestOwnReferenceLock,
+               negativeTestReleaseReference,
+               negativeTestGetReferenceParent
 )
