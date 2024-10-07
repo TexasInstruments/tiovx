af0653b0dcf9be835985c22c0b2c3ba4d59bf7c7
Shilpa John <x1120129@ti.com>
2024-10-07 16:41:12 +0530

    [TEST_CASE][UNTESTABLE][BRANCH][FRAMEWORK][DELAY]
    - Test case added: testOwnIsValidObject

--- conformance_tests/test_internal/test_delay.c
+++ conformance_tests/test_internal/test_delay.c
@@ -72,8 +72,70 @@ TEST(tivxInternaldelay, negativeTestVxAgeDelay)
 
 }
 
+TEST(tivxInternaldelay, testOwnIsValidObject)
+{
+    vx_context context = context_->vx_context_;
+    vx_array array = NULL;
+    vx_matrix matrix = 0;
+    vx_size capacity = 2;
+    vx_size count = 1;
+    vx_distribution dist = NULL;
+    vx_delay delay = NULL;
+    vx_threshold hyst = NULL;
+    const vx_enum matrix_type = VX_TYPE_INT8;
+    const vx_size matrix_rows = 3;
+    const vx_size matrix_cols = 2;
+    vx_uint32 src_width = 16;
+    vx_uint32 src_height = 32;
+    vx_uint32 dst_width = 128;
+    vx_uint32 dst_height = 64;
+    vx_remap map = 0;
+    vx_lut lut = 0;
+    vx_user_data_object src_user_data;
+    vx_char test_name[] = {'t', 'e', 's', 't', 'i', 'n', 'g'};
+    vx_uint32 udata = 0;
+
+    ASSERT_VX_OBJECT(array = vxCreateArray(context, VX_TYPE_KEYPOINT, capacity), VX_TYPE_ARRAY);
+    ASSERT_VX_OBJECT(delay = vxCreateDelay(context, (vx_reference)array, count), VX_TYPE_DELAY);
+    VX_CALL(vxReleaseDelay(&delay));
+    
+
+    ASSERT_VX_OBJECT(dist = vxCreateDistribution(context, 256, 0, 256), VX_TYPE_DISTRIBUTION);
+    ASSERT_VX_OBJECT(delay = vxCreateDelay(context, (vx_reference)dist, count), VX_TYPE_DELAY);
+    VX_CALL(vxReleaseDelay(&delay));
+
+    ASSERT_VX_OBJECT(hyst = vxCreateThreshold(context, VX_THRESHOLD_TYPE_RANGE, VX_TYPE_UINT8), VX_TYPE_THRESHOLD);
+    ASSERT_VX_OBJECT(delay = vxCreateDelay(context, (vx_reference)hyst, count), VX_TYPE_DELAY);
+    VX_CALL(vxReleaseDelay(&delay));
+
+    ASSERT_VX_OBJECT(matrix = vxCreateMatrix(context, matrix_type, matrix_cols, matrix_rows), VX_TYPE_MATRIX);
+    ASSERT_VX_OBJECT(delay = vxCreateDelay(context, (vx_reference)matrix, count), VX_TYPE_DELAY);
+    VX_CALL(vxReleaseDelay(&delay));
+
+    ASSERT_VX_OBJECT(map = vxCreateRemap(context, src_width, src_height, dst_width, dst_height), VX_TYPE_REMAP);
+    ASSERT_VX_OBJECT(delay = vxCreateDelay(context, (vx_reference)map, count), VX_TYPE_DELAY);
+    VX_CALL(vxReleaseDelay(&delay));
+
+    ASSERT_VX_OBJECT(lut = vxCreateLUT(context, VX_TYPE_UINT8, 256), VX_TYPE_LUT);
+    ASSERT_VX_OBJECT(delay = vxCreateDelay(context, (vx_reference)lut, count), VX_TYPE_DELAY);
+    VX_CALL(vxReleaseDelay(&delay));
+
+    ASSERT_VX_OBJECT(src_user_data = vxCreateUserDataObject(context, test_name, sizeof(vx_uint32), &udata), VX_TYPE_USER_DATA_OBJECT);
+    ASSERT_VX_OBJECT(delay = vxCreateDelay(context, (vx_reference)src_user_data, count), VX_TYPE_DELAY);
+    VX_CALL(vxReleaseDelay(&delay));
+
+    VX_CALL(vxReleaseArray(&array));
+    VX_CALL(vxReleaseDistribution(&dist));
+    VX_CALL(vxReleaseThreshold(&hyst));
+    VX_CALL(vxReleaseMatrix(&matrix));
+    VX_CALL(vxReleaseRemap(&map));
+    VX_CALL(vxReleaseLUT(&lut));
+    VX_CALL(vxReleaseUserDataObject(&src_user_data));
+
+}
 TESTCASE_TESTS(
     tivxInternaldelay,
-    negativeTestVxAgeDelay
+    negativeTestVxAgeDelay,
+    testOwnIsValidObject
 )
 
