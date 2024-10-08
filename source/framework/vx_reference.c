bfc8e4a7caf3bf1547ef06a12caf2f12453ebdd2
Maier Florian (XC-DX/EPO3) <Florian.Maier2@de.bosch.com>
2024-10-08 17:19:34 +0200

    correct a typo

--- source/framework/vx_reference.c
+++ source/framework/vx_reference.c
@@ -1042,7 +1042,7 @@ VX_API_ENTRY vx_status VX_API_CALL vxQueryReference(vx_reference ref, vx_enum at
                 }
                 break;
             case (vx_enum)VX_REFERENCE_ENQUEUE_COUNT: 
-                if (VX_CHECK_PARAM(ptr, size, vx_uint32*, 0x3U))
+                if (VX_CHECK_PARAM(ptr, size, vx_uint32, 0x3U))
                 {
                     *(vx_uint32 *)ptr = ref->obj_desc->num_enqueues; 
                 }
