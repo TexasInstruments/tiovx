3f06fea3c5a238965daeaa7144ae3c7a766f38b5
Lucas Weaver <l-weaver@ti.com>
2025-02-12 14:31:40 -0600

    [TIMEOUT] Updating to remove TIVX timeout and migrate to VX timeout
    -Also fixing a couple MISRA-C issues

--- source/framework/vx_graph_pipeline.c
+++ source/framework/vx_graph_pipeline.c
@@ -221,7 +221,7 @@ static vx_status ownDecrementEnqueueCount(vx_reference ref)
     ref->obj_desc->flags &= ~TIVX_OBJ_DESC_DATA_REF_GRAPH_PARAM_ENQUEUED;
     if (ref->obj_desc->num_enqueues > 0U)
     {
-        ref->obj_desc->num_enqueues --;
+        ref->obj_desc->num_enqueues = ref->obj_desc->num_enqueues - 1U;
         status = (vx_status)VX_SUCCESS;
     }
     else if (NULL == ref->delay)
@@ -464,7 +464,7 @@ static vx_status ownGraphParameterEnqueueReadyRef(vx_graph graph,
                                 /* if this parameter is on a replicated node, we need to increment the queue counter
                                    and set the enqueued flag for all elements of the corresponding container */
                                 /* Increment the queue counter */
-                                objd->num_enqueues ++;
+                                objd->num_enqueues = objd->num_enqueues + 1U;
                                 if ((vx_bool)vx_false_e == is_input)
                                 {
                                     /* not an input, it's output or bidirectional so set the exclusive use flag */
@@ -478,7 +478,7 @@ static vx_status ownGraphParameterEnqueueReadyRef(vx_graph graph,
                                         /* loop over the replicats object descriptor */
                                         tivx_obj_desc_t *odi = ref_list[i]->obj_desc;
                                         /* Increment the queue counter */
-                                        odi->num_enqueues ++;
+                                        odi->num_enqueues = odi->num_enqueues + 1U;
                                         if ((vx_bool)vx_false_e == is_input)
                                         {
                                             /* not an input, it's output or bidirectional so set the exclusive use flag */
