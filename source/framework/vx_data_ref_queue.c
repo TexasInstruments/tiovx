raphacan/tiovx-bosch-dummy

[TIOVX-2108] Multi enqueue and graph events implementation
Signed-off-by: Cano Raphael(XC-AS/EPO3) <Raphael.Cano2@de.bosch.com>

source/framework/vx_data_ref_queue.c
@@ -269,6 +269,16 @@ vx_status ownDataRefQueueSendRefConsumedEvent(tivx_data_ref_queue ref, uint64_t 
             }
 #endif
         }
+        if ((vx_bool)vx_true_e == ref->is_enable_send_ref_consumed_graph_event)
+        {
+            status = ownEventQueueAddEvent(&ref->graph->graph_event_queue,
+                        (vx_enum)VX_EVENT_GRAPH_PARAMETER_CONSUMED, timestamp, ref->graph->parameters[ref->graph_parameter_index].graph_consumed_app_value,
+                        (uintptr_t)ref->graph, (uintptr_t)ref->graph_parameter_index, (uintptr_t)0);
+            if((vx_status)VX_SUCCESS != status)
+            {
+                VX_PRINT(VX_ZONE_ERROR,"Failed to add event to graph event queue\n");
+            }
+        }
     }
     else
     {
@@ -384,6 +394,7 @@ tivx_data_ref_queue tivxDataRefQueueCreate(vx_graph graph, const tivx_data_ref_q
             ref->release_q_obj_desc_id = (vx_enum)TIVX_OBJ_DESC_INVALID;
 
             ref->is_enable_send_ref_consumed_event = prms->is_enable_send_ref_consumed_event;
+            ref->is_enable_send_ref_consumed_graph_event = prms->is_enable_send_ref_consumed_graph_event;
             ref->enable_user_queueing = prms->enable_user_queueing;
             ref->graph = graph;
             ref->graph_parameter_index = prms->graph_parameter_index;

TexasInstruments/tiovx

source/framework/vx_data_ref_queue.c
@@ -268,7 +268,7 @@ vx_status ownDataRefQueueSendRefConsumedEvent(tivx_data_ref_queue ref, uint64_t 
             }
 /* LDRA_JUSTIFY_END */
         }
-        if(ref->is_enable_send_ref_consumed_event != 0)
+        if((vx_bool)vx_true_e == ref->is_enable_send_ref_consumed_event)
         {
             status = ownEventQueueAddEvent(&ref->base.context->event_queue,
                         (vx_enum)VX_EVENT_GRAPH_PARAMETER_CONSUMED, timestamp, ref->graph->parameters[ref->graph_parameter_index].graph_consumed_app_value,
@@ -282,6 +282,19 @@ vx_status ownDataRefQueueSendRefConsumedEvent(tivx_data_ref_queue ref, uint64_t 
                 VX_PRINT(VX_ZONE_ERROR,"Failed to add event to event queue\n");
             }
 /* LDRA_JUSTIFY_END */
+        }
+        if ((vx_bool)vx_true_e == ref->is_enable_send_ref_consumed_graph_event)
+        {
+            status = ownEventQueueAddEvent(&ref->graph->event_queue,
+                        (vx_enum)VX_EVENT_GRAPH_PARAMETER_CONSUMED, timestamp, ref->graph->parameters[ref->graph_parameter_index].graph_consumed_app_value,
+                        (uintptr_t)ref->graph, (uintptr_t)ref->graph_parameter_index, (uintptr_t)0);
+#ifdef LDRA_UNTESTABLE_CODE
+/* same deviation as above */
+            if((vx_status)VX_SUCCESS != status)
+            {
+                VX_PRINT(VX_ZONE_ERROR,"Failed to add event to graph event queue\n");
+            }
+#endif
         }
     }
     else
@@ -409,6 +422,7 @@ tivx_data_ref_queue tivxDataRefQueueCreate(vx_graph graph, const tivx_data_ref_q
             ref->release_q_obj_desc_id = (vx_enum)TIVX_OBJ_DESC_INVALID;
 
             ref->is_enable_send_ref_consumed_event = prms->is_enable_send_ref_consumed_event;
+            ref->is_enable_send_ref_consumed_graph_event = prms->is_enable_send_ref_consumed_graph_event;
             ref->enable_user_queueing = prms->enable_user_queueing;
             ref->graph = graph;
             ref->graph_parameter_index = prms->graph_parameter_index;