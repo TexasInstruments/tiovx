e30fab40c403a756179dd916260c6c171ce09db9
Brock Carey <brock@ti.com>
2025-11-14 10:08:21 -0600

    [INCLUDE][TI] Adding missing SOC_NAME define to J742s2
    Signed-off-by: Brock Carey <brock@ti.com>

--- include/TI/soc/tivx_soc_j742s2.h
+++ include/TI/soc/tivx_soc_j742s2.h
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2024 Texas Instruments Incorporated
+ * Copyright (c) 2024-2025 Texas Instruments Incorporated
  *
  * All rights reserved not granted herein.
  *
@@ -84,6 +84,9 @@
 #ifndef C7XMMA_COUNT
 #define C7XMMA_COUNT  (2U)
 #endif
+
+/* Name of this SoC */
+#define SOC_NAME "j742s2"
 
 #ifdef __cplusplus
 extern "C" {
