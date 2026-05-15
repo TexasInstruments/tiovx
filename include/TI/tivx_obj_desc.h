TexasInstruments/tiovx

commit 0d78cdd6300610be63cf49966b0e90e0c4f36208

[TIOVX-2548] Adding new image attributes conformant with OpenVX 1.3+

include/TI/tivx_obj_desc.h
@@ -556,17 +556,8 @@ typedef struct _tivx_obj_desc_image
     volatile uint16_t parent_id;
     /*! \brief reserved for 64b alignment */
     volatile uint16_t rsv[1];
-    /*! \brief the value to use to fill for a uniform image.
-     *
-     * bit 0.. 7 - Component 0 - R or Y or U8.
-     * bit 8..15 - Component 1 - G or U.
-     * bit16..23 - Component 2 - B or V.
-     * bit24..31 - Component 3 - X.
-     *
-     * bit0..15 - U16, S16.
-     * bit0..31 - U32, S32.
-     */
-    volatile uint32_t uniform_image_pixel_value;
+    /*! \brief Value of the pixel in a uniform image (uniform only) */
+    volatile vx_pixel_value_t uniform_image_pixel_value;
     /*! \brief method by which image was created, see \ref tivx_image_create_type_e */
     volatile uint32_t create_type;
     /*! \brief image plane addressing parameters */

source/framework/vx_image.c
@@ -950,7 +950,8 @@ static void ownInitImage(vx_image image, vx_uint32 width, vx_uint32 height, vx_d
     image->channel_plane = 0;
     image->stride_y_alignment = TIVX_DEFAULT_STRIDE_Y_ALIGN;
 
-    obj_desc->uniform_image_pixel_value = 0;
+    obj_desc->uniform_image_pixel_value = (vx_pixel_value_t){0};
+
     obj_desc->width = width;
     obj_desc->height = height;
     obj_desc->format = format;
@@ -1865,6 +1866,8 @@ VX_API_ENTRY vx_image VX_API_CALL vxCreateUniformImage(vx_context context, vx_ui
                 /* lock the image from being modified again! */
                 ((tivx_obj_desc_image_t *)image->base.obj_desc)->create_type =
                     (vx_enum)TIVX_IMAGE_UNIFORM;
+                ((tivx_obj_desc_image_t *)image->base.obj_desc)->uniform_image_pixel_value =
+                    *value;
             }
         }
     }
@@ -2198,6 +2201,37 @@ VX_API_ENTRY vx_status VX_API_CALL vxQueryImage(vx_image image, vx_enum attribut
                 else
                 {
                     VX_PRINT(VX_ZONE_ERROR, "query image memory type failed\n");
+                    status = (vx_status)VX_ERROR_INVALID_PARAMETERS;
+                }
+                break;
+            case (vx_enum)VX_IMAGE_IS_UNIFORM:
+                if (VX_CHECK_PARAM(ptr, size, vx_bool, 0x3U))
+                {
+                    *(vx_bool *)ptr = (obj_desc->create_type == (uint32_t)TIVX_IMAGE_UNIFORM) ? 
+                        (vx_bool)vx_true_e : (vx_bool)vx_false_e;
+                }
+                else
+                {
+                    VX_PRINT(VX_ZONE_ERROR, "query image is uniform failed\n");
+                    status = (vx_status)VX_ERROR_INVALID_PARAMETERS;
+                }
+                break;
+            case (vx_enum)VX_IMAGE_UNIFORM_VALUE:
+                if (VX_CHECK_PARAM(ptr, size, vx_pixel_value_t, 0x3U))
+                {
+                    if (obj_desc->create_type != (uint32_t)TIVX_IMAGE_UNIFORM)
+                    {
+                        VX_PRINT(VX_ZONE_ERROR, "Attribute VX_IMAGE_UNIFORM_VALUE not supported for non-uniform images\n");
+                        status = (vx_status)VX_ERROR_NOT_SUPPORTED;
+                    } 
+                    else
+                    {
+                        *(vx_pixel_value_t *)ptr = obj_desc->uniform_image_pixel_value;
+                    }
+                }
+                else
+                {
+                    VX_PRINT(VX_ZONE_ERROR, "query image uniform value failed\n");
                     status = (vx_status)VX_ERROR_INVALID_PARAMETERS;
                 }
                 break;

conformance_tests/test_conformance/test_vximage.c
@@ -1217,6 +1217,8 @@ TEST_WITH_ARG(Image, testUniformImage, format_arg,
     CT_Image   ctimg   = 0;
     CT_Image   refimg  = 0;
     int i;
+    vx_pixel_value_t uniform_value = {0};
+    vx_bool is_uniform;
 
     vx_pixel_value_t vals;
 
@@ -1315,6 +1317,13 @@ TEST_WITH_ARG(Image, testUniformImage, format_arg,
     };
 
     EXPECT_EQ_CTIMAGE(refimg, ctimg);
+
+    ASSERT_EQ_VX_STATUS(VX_SUCCESS, vxQueryImage(image, VX_IMAGE_IS_UNIFORM,  &is_uniform,  sizeof(is_uniform)));
+    ASSERT_EQ_INT((vx_bool)vx_true_e, is_uniform);
+
+    ASSERT_EQ_VX_STATUS(VX_SUCCESS, vxQueryImage(image, VX_IMAGE_UNIFORM_VALUE,  &uniform_value,  sizeof(uniform_value)));
+    /* vals only initialized to first four bytes so can compare with U32 value */
+    ASSERT_EQ_INT(uniform_value.U32, vals.U32);
 
     VX_CALL(vxReleaseImage(&image));
     ASSERT(image == 0);
@@ -1658,6 +1667,8 @@ TEST(Image, testQueryImage)
     vx_enum space = 0;
     vx_enum range = 0;
     vx_enum memory_type = 0;
+    vx_pixel_value_t uniform_value = {0};
+    vx_bool is_uniform = (vx_bool)vx_true_e;
 
     image = vxCreateImage(context, 640, 480, VX_DF_IMAGE_U8);
 
@@ -1689,6 +1700,11 @@ TEST(Image, testQueryImage)
 */
     ASSERT_EQ_VX_STATUS(VX_SUCCESS, vxQueryImage(image, VX_IMAGE_MEMORY_TYPE,  &memory_type,  sizeof(memory_type)));
     ASSERT_EQ_INT(VX_MEMORY_TYPE_NONE, memory_type);
+
+    ASSERT_EQ_VX_STATUS(VX_SUCCESS, vxQueryImage(image, VX_IMAGE_IS_UNIFORM,  &is_uniform,  sizeof(is_uniform)));
+    ASSERT_EQ_INT((vx_bool)vx_false_e, is_uniform);
+
+    ASSERT_EQ_VX_STATUS(VX_ERROR_NOT_SUPPORTED, vxQueryImage(image, VX_IMAGE_UNIFORM_VALUE,  &uniform_value,  sizeof(uniform_value)));
 
     VX_CALL(vxReleaseImage(&image));
 } /* testQueryImage() */

conformance_tests/test_tiovx/test_image.c
@@ -254,6 +254,8 @@ TEST(tivxImage, negativeTestQueryImage)
     ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_PARAMETERS, vxQueryImage(img, VX_IMAGE_RANGE, &size, size));
     ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_PARAMETERS, vxQueryImage(img, VX_IMAGE_SIZE, &size, size));
     ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_PARAMETERS, vxQueryImage(img, VX_IMAGE_MEMORY_TYPE, &size, size));
+    ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_PARAMETERS, vxQueryImage(img, VX_IMAGE_IS_UNIFORM, &size, size));
+    ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_PARAMETERS, vxQueryImage(img, VX_IMAGE_UNIFORM_VALUE, &size, size));
     ASSERT_EQ_VX_STATUS(VX_ERROR_NOT_SUPPORTED, vxQueryImage(img, attribute, &size, size));
     ASSERT_EQ_VX_STATUS(VX_SUCCESS, vxQueryImage(img, VX_IMAGE_SIZE, &size, sizeof(vx_size)));
     ASSERT_EQ_VX_STATUS(VX_ERROR_INVALID_PARAMETERS, vxQueryImage(img, TIVX_IMAGE_STRIDE_Y_ALIGNMENT, &size, size));

include/VX/vx_types.h
@@ -916,6 +916,10 @@ enum vx_image_attribute_e {
     /*! \brief Queries memory type if created using vxCreateImageFromHandle. If vx_image was not created using
         vxCreateImageFromHandle, VX_MEMORY_TYPE_NONE is returned. Use a <tt>\ref vx_memory_type_e</tt> parameter. */
     VX_IMAGE_MEMORY_TYPE = VX_ATTRIBUTE_BASE(VX_ID_KHRONOS, VX_TYPE_IMAGE) + 0x7,
+    /*! \brief Queries if an image is uniform. Read-only. Use a <tt>\ref vx_bool</tt> parameter. */
+    VX_IMAGE_IS_UNIFORM = VX_ATTRIBUTE_BASE(VX_ID_KHRONOS, VX_TYPE_IMAGE) + 0x8,
+    /*! \brief Queries the image uniform value if any. Read-only. Use a <tt>\ref vx_pixel_value_t</tt> parameter. */
+    VX_IMAGE_UNIFORM_VALUE = VX_ATTRIBUTE_BASE(VX_ID_KHRONOS, VX_TYPE_IMAGE) + 0x9,
 };
 
 /*! \brief The scalar attributes list.

tools/PyTIOVX/tiovx/attribute.py
@@ -88,6 +88,10 @@ class ImageAttribute(Enum) :
     SIZE        = ('size', 'vx_size')
     ## OpenVX equivalent = \ref VX_IMAGE_MEMORY_TYPE
     MEMORY_TYPE = ('memory_type', 'vx_enum')
+    ## OpenVX equivalent = \ref VX_IMAGE_IS_UNIFORM
+    IS_UNIFORM = ('is_uniform', 'vx_bool')
+    ## OpenVX equivalent = \ref VX_IMAGE_UNIFORM_VALUE
+    UNIFORM_VALUE = ('uniform_value', 'vx_pixel_value_t')
 
     def vx_enum_name(attr) :
         return "VX_IMAGE_" + attr.name

commit ae8443fae3b800e0b84cafd5cf14566dd99764d9

[TIOVX-2548] Fixing regression due to alignment errors of the image object descriptor  - Introduced in 0d78cdd6300610be63cf49966b0e90e0c4f36208 which changed a field from vx_uint32 to vx_pixel_value_t  - Realigning the tivx_obj_desc for images to be 64/32b aligned after adding 12 bytes  - In the case of unalignment, the vx_pixel_value_t uniform_pixel_value_t assignment in vxCreateUniformImage is protected with a smart memcpy for obj descs

include/TI/tivx_obj_desc.h
@@ -555,7 +555,7 @@ typedef struct _tivx_obj_desc_image
     /*! \brief object descriptor ID of parent (if any) */
     volatile uint16_t parent_id;
     /*! \brief reserved for 64b alignment */
-    volatile uint16_t rsv[1];
+    volatile uint16_t rsv[3];
     /*! \brief Value of the pixel in a uniform image (uniform only) */
     volatile vx_pixel_value_t uniform_image_pixel_value;
     /*! \brief method by which image was created, see \ref tivx_image_create_type_e */

source/framework/vx_image.c
@@ -1864,10 +1864,9 @@ VX_API_ENTRY vx_image VX_API_CALL vxCreateUniformImage(vx_context context, vx_ui
             if (vxGetStatus(vxCastRefFromImage(image)) == (vx_status)VX_SUCCESS)
             {
                 /* lock the image from being modified again! */
-                ((tivx_obj_desc_image_t *)image->base.obj_desc)->create_type =
-                    (vx_enum)TIVX_IMAGE_UNIFORM;
-                ((tivx_obj_desc_image_t *)image->base.obj_desc)->uniform_image_pixel_value =
-                    *value;
+                tivx_obj_desc_image_t *uni_img_obj_desc = (tivx_obj_desc_image_t *)image->base.obj_desc;
+                uni_img_obj_desc->create_type = (vx_enum)TIVX_IMAGE_UNIFORM;
+                tivx_obj_desc_memcpy(&uni_img_obj_desc->uniform_image_pixel_value, (volatile void *) value, (uint32_t)sizeof(vx_pixel_value_t));
             }
         }
     }